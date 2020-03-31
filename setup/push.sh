dfcfb45fe6dd2adbe42951593fd28326e6d2075d
Nipinium <nipinium@gmail.com>
2020-03-31 14:43:08 +0700

    fix services

--- setup/push.sh
+++ setup/push.sh
@@ -2,3 +2,4 @@
 
 rsync -azi data/dic-out/ nipin@ssh.nipin.xyz:web/chivi/data/dic-out/
 rsync -azi data/txt-out/ nipin@ssh.nipin.xyz:web/chivi/data/txt-out/
+rsync -azi data/txt-tmp/yousuu nipin@ssh.nipin.xyz:web/chivi/data/txt-tmp/
