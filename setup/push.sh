1e12c1436994ca45198e16e0494878b532c5c6e8
Nipinium <nipinium@gmail.com>
2020-04-04 05:38:45 +0700

    ditto

--- setup/push.sh
+++ setup/push.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rsync -azi data/dic-out/ nipin@ssh.nipin.xyz:web/chivi/data/dic-out/
-rsync -azi data/txt-out/ nipin@ssh.nipin.xyz:web/chivi/data/txt-out/
-rsync -azi data/txt-tmp/yousuu nipin@ssh.nipin.xyz:web/chivi/data/txt-tmp/
-rsync -azi data/txt-tmp/chtexts/zhwenpg nipin@ssh.nipin.xyz:web/chivi/data/txt-tmp/chtexts/
+rsync -azi data/dic-out/ nipin@ssh.chivi.xyz:web/chivi/data/dic-out/
+rsync -azi data/txt-out/ nipin@ssh.chivi.xyz:web/chivi/data/txt-out/
+rsync -azi data/txt-tmp/yousuu nipin@ssh.chivi.xyz:web/chivi/data/txt-tmp/
+rsync -azi data/txt-tmp/chtexts/zhwenpg nipin@ssh.chivi.xyz:web/chivi/data/txt-tmp/chtexts/
