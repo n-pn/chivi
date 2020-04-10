51c14ce3bb590b717af4f166d2b7383f58e79250
Nipinium <nipinium@gmail.com>
2020-04-11 01:29:09 +0700

    upgrade packages

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rsync -Prltvc --chmod=Dg+s,ug+w,Fo-w,+X data/dic-out nipin@ssh.chivi.xyz:web/chivi/data/
-rsync -Prltvc --chmod=Dg+s,ug+w,Fo-w,+X data/txt-out nipin@ssh.chivi.xyz:web/chivi/data/
-rsync -Prltvc data/txt-tmp/yousuu nipin@ssh.chivi.xyz:web/chivi/data/txt-tmp/
-rsync -Prltvc --chmod=Dg+s,ug+w,Fo-w,+X data/txt-tmp/chtexts/zhwenpg nipin@ssh.chivi.xyz:web/chivi/data/txt-tmp/chtexts/
+rsync -azvW --chmod=Dg+s,ug+w,Fo-w,+X data/dic-out nipin@ssh.chivi.xyz:web/chivi/data/
+rsync -azvW --chmod=Dg+s,ug+w,Fo-w,+X data/txt-out nipin@ssh.chivi.xyz:web/chivi/data/
+rsync -azvW data/txt-tmp/yousuu nipin@ssh.chivi.xyz:web/chivi/data/txt-tmp/
+rsync -azvW --chmod=Dg+s,ug+w,Fo-w,+X data/txt-tmp/chtexts/zhwenpg nipin@ssh.chivi.xyz:web/chivi/data/txt-tmp/chtexts/
