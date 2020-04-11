c26328c2a17666f3233621971b36e58c93c344db
Nipinium <nipinium@gmail.com>
2020-04-11 09:43:19 +0700

    update yousuu serials crawler

--- tasks/yousuu/serials.rb
+++ tasks/yousuu/serials.rb
@@ -6,40 +6,7 @@ require "parallel"
 require "colorize"
 require "fileutils"
 
-# ## Core
-
-ROOT_DIR = "data/txt-inp/yousuu/serials/"
-OUT_FILE = "#{ROOT_DIR}/%i.json"
-BOOK_URL = "https://www.yousuu.com/api/book/%i?t=%i"
-
-def out_file(book_id)
-  OUT_FILE % book_id
-end
-
-def to_unix_ms(time = Time.now)
-  (time.to_f * 1000).round
-end
-
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
-
-def fetch_meta(book_id, proxy, verbose = false)
-    begin
-      url = BOOK_URL % [book_id, to_unix_ms(Time.now)]
-      puts "HIT: [#{url}], proxy: [#{proxy}]".blue if verbose
-
-      body = URI.open(url, proxy: proxy, read_timeout: 10, "User-Agent" => USER_AGENT) { |f| f.read }
-      raise "Malformed!" unless body.include?("{\"success\":true,") || body.include?("未找到该图书")
-
-      puts "- proxy [#{proxy}] worked!".green if verbose
-      File.write out_file(book_id), body
-
-      {status: :ok, proxy: proxy, book_id: book_id}
-    rescue => err
-      puts "- proxy [#{proxy}] not working, reason: #{err}".red if verbose
-
-      {status: :err, proxy: proxy, book_id: book_id}
-    end
-end
+VERBOSE = ARGV.include?("verbose")
 
 # # Prepare proxies
 
@@ -53,13 +20,6 @@ def load_proxies(file)
   proxies.uniq!
   puts "- loaded #{proxies.size} proxies from [#{file}]".blue
   proxies
-end
-
-OUTDATED = 3600 * 24 * 7 # 5 days
-def file_outdated?(file, span = OUTDATED)
-  return true unless File.exists?(file)
-  mtime = File.mtime(file).to_i
-  Time.now.to_i - mtime > span
 end
 
 PROXY_TIME = Time.new.to_i / (3600 * 6)
@@ -83,23 +43,64 @@ end
 proxies.uniq!
 # proxies = proxies.shuffle
 
+# ## Core
+
+ROOT_DIR = "data/txt-inp/yousuu/serials/"
+OUT_FILE = "#{ROOT_DIR}/%i.json"
+BOOK_URL = "https://www.yousuu.com/api/book/%i?t=%i"
+
+OUTDATED = 3600 * 24 * 3 # 3 days
+def outdated?(file)
+  return true unless File.exists?(file)
+
+  data = File.read(file)
+  return false if data.include?("未找到该图书")
+
+  json = JSON.parse(data)
+
+  status = json["data"]["bookInfo"]["status"] || 0
+  outdated = OUTDATED * (status + 1)
+
+  score = json["data"]["bookInfo"]["score"] || 0
+  outdated *= 2 if score == 0
+
+  mtime = File.mtime(file).to_i
+  Time.now.to_i - mtime > outdated
+end
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
+
+def fetch_meta(book_id, proxy)
+  file = OUT_FILE % book_id
+  return :skip unless outdated?(file)
+
+  url = BOOK_URL % [book_id, (Time.now.to_f * 1000).round]
+  puts "HIT: [#{url}], proxy: [#{proxy}]".blue if VERBOSE
+
+  body = URI.open(url, proxy: proxy, read_timeout: 10, "User-Agent" => USER_AGENT) { |f| f.read }
+  raise "Malformed!" unless body.include?("{\"success\":true,") || body.include?("未找到该图书")
+
+  File.write(file, body)
+  File.open(PROXY_FILE, "a") { |f| f.puts proxy.sub("http://", "") }
+
+  puts "- proxy [#{proxy}] worked!".green if VERBOSE
+  :done
+rescue => err
+  puts "- proxy [#{proxy}] not working, reason: #{err}".red if VERBOSE
+  :error
+end
+
 # # Prepare serials
 
-TOTAL   = 212000 # max book_id
-VERBOSE = ARGV.include?("verbose")
-
-puts "Check for outdated entries...".blue
-
-fetched = Dir.glob("#{ROOT_DIR}/*.json").reject { |file| file_outdated?(file) }
-existed = Set.new fetched.map { |x| File.basename(x, ".json").to_i }
-serials = (1..TOTAL).reject { |x| existed.include?(x) }
+TOTAL = 212000 # max book_id
+serials = (1..TOTAL).to_a
 
 if ARGV.include?("shuffle")
   puts "SHUFFLE INPUT!".yellow
-  serials = serials.shuffle
+  serials.shuffle!
 elsif ARGV.include?("reverse")
   puts "REVERSE INPUT!".yellow
-  serials = serials.reverse
+  serials.reverse!
 end
 
 # # Crawling!
@@ -114,20 +115,22 @@ until proxies.empty? || serials.empty?
   Parallel.each_with_index(proxies.take(serials.size), in_threads: 20) do |proxy, idx|
     book_id = serials.pop
 
-    res = fetch_meta(book_id, proxy, VERBOSE)
+    res = fetch_meta(book_id, proxy)
+    message = "- [#{idx + 1}/#{proxies.size}]: #{res}! (book_id: #{book_id}, proxy: #{proxy})"
 
-    if res[:status] == :ok
-      working << res[:proxy]
-
-      puts "- [#{idx + 1}/#{proxies.size}] ok! (book_id: #{res[:book_id]}, proxy: #{res[:proxy]})".green
-      File.open(PROXY_FILE, "a") { |f| f.puts proxy.sub("http://", "") }
-    else
-      puts "- [#{idx + 1}/#{proxies.size}] error! (book_id: #{res[:book_id]}, proxy: #{res[:proxy]})".red
-      failure << res[:book_id]
+    case res
+    when :skip
+      working << proxy
+    when :done
+      working << proxy
+      puts message.green unless VERBOSE
+    when :error
+      failure << book_id
+      puts message.red unless VERBOSE
     end
   end
 
   step += 1
   proxies = working
-  serials.concat failure
+  serials.concat(failure)
 end
