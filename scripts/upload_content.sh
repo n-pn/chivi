1b2956fb8fb658fa25936908deab7e36ed1ad176
Nipinium <nipinium@gmail.com>
2020-05-27 09:02:40 +0700

    fix upload content overriding user dicts

--- scripts/upload_content.sh
+++ scripts/upload_content.sh
@@ -3,7 +3,15 @@
 INP=data
 OUT=nipin@ssh.chivi.xyz:web/chivi
 
-rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/cv_dicts" "$OUT/data/"
+rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/cv_dicts/cc_cedict" "$OUT/data/cv_dicts"
+rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/cv_dicts/trungviet" "$OUT/data/cv_dicts"
+rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/cv_dicts/hanviet" "$OUT/data/cv_dicts"
+rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/cv_dicts/hantrad" "$OUT/data/cv_dicts"
+rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/cv_dicts/pinyins" "$OUT/data/cv_dicts"
+rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/cv_dicts/tradsim" "$OUT/data/cv_dicts"
+rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/cv_dicts/shared_base" "$OUT/data/cv_dicts"
+rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/cv_dicts/unique_base" "$OUT/data/cv_dicts"
+
 rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/book_infos" "$OUT/data/"
 rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/vp_lists" "$OUT/data/"
 rsync -azi --chmod=Dg+s,ug+w,Fo-w,+X "$INP/indexing" "$OUT/data/"
