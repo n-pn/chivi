8c291cb8b2711106f7160d9b84222f00c4adcfe8
Nipinium <nipinium@gmail.com>
2020-05-31 11:52:18 +0700

    fix cover

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -247,8 +247,8 @@
     z-index: 1;
 
     img {
-      min-width: 100%;
-      height: 100%;
+      max-width: 100%;
+      max-height: 100%;
       object-fit: cover;
       @include radius();
     }
