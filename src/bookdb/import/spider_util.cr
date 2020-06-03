0fda6d185e38c015b5e8010e46be48494f98590d
Nipinium <nipinium@gmail.com>
2020-06-03 20:48:35 +0700

    wip: rewrite import chap text

---
+++ src/bookdb/import/spider_util.cr
@@ -0,0 +1,34 @@
+require "colorize"
+require "http/client"
+
+module SpiderUtil
+  def self.fetch_html(url : String) : String
+    puts "- HIT: #{url.colorize(:grey)}"
+    fetch_html(url, tls_for(url), encoding_for(url))
+  end
+
+  def self.fetch_html(url : String, tls : OpenSSL::SSL::Context::Client?, encoding : String) : String?
+    HTTP::Client.get(url, tls: tls) do |res|
+      res.body_io.set_encoding(encoding, invalid: :skip)
+      res.body_io.try(&.gets_to_end)
+    end
+  rescue err
+    puts "- error fetching `#{url.colorize(:red)}`: [#{err.class}] #{err.colorize(:red)}"
+    sleep 500.milliseconds
+    fetch_html(url, tls, encoding)
+  end
+
+  def self.encoding_for(url : String) : String
+    {"jx.la", "hetushu", "paoshu8", "zhwenpg"}.each do |site|
+      return "UTF-8" if url.includes?(site)
+    end
+
+    "GBK"
+  end
+
+  TLS = OpenSSL::SSL::Context::Client.insecure
+
+  def self.tls_for(url : String) : OpenSSL::SSL::Context::Client?
+    url.starts_with?("https") ? TLS : nil
+  end
+end
