91a911a194c248e0c9b4c89b65be1f7e990ff029
Nipinium <nipinium@gmail.com>
2020-07-25 19:50:14 +0700

    upload userdb

--- etc/script/upload/assets.sh
+++ etc/script/upload/assets.sh
@@ -4,7 +4,7 @@ OUT=nipin@ssh.nipin.xyz:web/chivi
 
 rsync -azi --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r "var/dictdb" "$OUT/var/"
 rsync -azi --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r "var/bookdb" "$OUT/var/"
-# rsync -azi --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r "var/userdb" "$OUT/var/"
+rsync -azi --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r "var/userdb" "$OUT/var/"
 rsync -azi --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r "web/public" "$OUT/web/"
 
 # INP=var/._old_info
