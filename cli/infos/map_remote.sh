75030341e92bfcf5a03fa3fcf3828e1bdf976608
Nipinium <nipinium@gmail.com>
2020-07-28 02:21:54 +0700

    update book ids

--- cli/infos/map_remote.sh
+++ cli/infos/map_remote.sh
@@ -1,9 +1,9 @@
-time bin/map_remote -s hetushu -f 1 -u 4868
+time bin/map_remote -s hetushu -f 1 -u 4879
 time bin/map_remote -s rengshu -f 1 -u 4275
-time bin/map_remote -s xbiquge -f 1 -u 53053
-time bin/map_remote -s nofff -f 1 -u 73783
-time bin/map_remote -s duokan8 -f 1 -u 26139
-time bin/map_remote -s 69shu -f 1 -u 32472
-time bin/map_remote -s paoshu8 -f 1 -u 148531
-time bin/map_remote -s jx_la -f 1 -u 254238
-# time bin/map_remote -s qu_la -f 1 -u 253059
+time bin/map_remote -s xbiquge -f 1 -u 53057
+time bin/map_remote -s nofff -f 1 -u 75435
+time bin/map_remote -s duokan8 -f 1 -u 27646
+time bin/map_remote -s 69shu -f 1 -u 33452
+time bin/map_remote -s paoshu8 -f 1 -u 148852
+time bin/map_remote -s jx_la -f 1 -u 254364
+# time bin/map_remote -s qu_la -f 1 -u 253996
