0acaa738d78730750d326160db6af0a9eac3129e
Nipinium <nipinium@gmail.com>
2020-07-28 16:43:28 +0700

    turn `secure` mode on in production

--- src/server.cr
+++ src/server.cr
@@ -6,6 +6,7 @@ Kemal::Session.config do |config|
   config.timeout = 3.days
   config.engine = Kemal::Session::FileEngine.new({:sessions_dir => "tmp/"})
   config.gc_interval = 5.minutes # 5 minutes
+  config.secure = Kemal.config.env == "production"
 end
 
 module Server
