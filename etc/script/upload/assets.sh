490affc53ffc9a9e6ffd8f64d0d5bb26459d4c7d
Nipinium <nipinium@gmail.com>
2020-07-31 02:19:48 +0700

    fix script

--- etc/script/upload/assets.sh
+++ etc/script/upload/assets.sh
@@ -4,8 +4,9 @@ OUT=nipin@ssh.nipin.xyz:web/chivi
 
 rsync -azui --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --exclude '*.log' "var/dictdb" "$OUT/var/"
 rsync -azui --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r "var/bookdb" "$OUT/var/"
-# rsync -azi --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r "var/userdb" "$OUT/var/"
 rsync -azui --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r "web/public" "$OUT/web/"
+
+# rsync -azi --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r "var/userdb" "$OUT/var/"
 
 # INP=var/._old_info
 # OUT=nipin@ssh.chivi.xyz:web/chivi/data
