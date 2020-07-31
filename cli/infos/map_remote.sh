53e51caada46f503b7398722a915042c56e5750e
Nipinium <nipinium@gmail.com>
2020-08-01 04:03:20 +0700

    update book sources

--- cli/infos/map_remote.sh
+++ cli/infos/map_remote.sh
@@ -1,9 +1,9 @@
-time bin/map_remote -s hetushu -f 1 -u 4879
+time bin/map_remote -s hetushu -f 1 -u 4887
 time bin/map_remote -s rengshu -f 1 -u 4275
-time bin/map_remote -s xbiquge -f 1 -u 53057
-time bin/map_remote -s nofff -f 1 -u 75435
-time bin/map_remote -s duokan8 -f 1 -u 27646
+time bin/map_remote -s xbiquge -f 1 -u 53061
+time bin/map_remote -s nofff -f 1 -u 76794
+time bin/map_remote -s duokan8 -f 1 -u 28094
 time bin/map_remote -s 69shu -f 1 -u 33452
-time bin/map_remote -s paoshu8 -f 1 -u 148852
-time bin/map_remote -s jx_la -f 1 -u 254364
-# time bin/map_remote -s qu_la -f 1 -u 253996
+time bin/map_remote -s paoshu8 -f 1 -u 149155
+time bin/map_remote -s jx_la -f 1 -u 254593
+# time bin/map_remote -s qu_la -f 1 -u 254854
