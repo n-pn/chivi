2c849dfbd809f876cc730fbdab75793a4fb87007
Nipinium <nipinium@gmail.com>
2020-08-03 00:43:14 +0700

    remove dead server

--- etc/script/backup/assets.sh
+++ etc/script/backup/assets.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
 
-INP_OLD=nipin@ssh.chivi.xyz:web/chivi/data
-OUT_OLD=var/._old_info
-
-rsync -azi "$INP_OLD/cv_dicts/*" "$OUT_OLD/cv_dicts"
-rsync -azui "$INP/zh_texts/*" "$OUT/zh_texts"
-
 INP=nipin@ssh.nipin.xyz:web/chivi/var/
 OUT=var/
 
