a5666161a12401b142d977571980bdc36de637bd
Nipinium <nipinium@gmail.com>
2020-08-02 21:50:22 +0700

    using macro to cleanup repeating codes

--- src/libcv.cr
+++ src/libcv.cr
@@ -4,57 +4,41 @@ module Libcv
   extend self
 
   def tradsim(input : String)
-    dict = Libcv::Library.tradsim
+    dict = Library.tradsim
     Convert.translit(input, dict, false, false)
   end
 
   def tradsim(lines : Array(String))
-    dict = Libcv::Library.tradsim
+    dict = Library.tradsim
     lines.map { |line| Convert.translit(line, dict, false, false) }
   end
 
-  def hanviet(input : String, apply_cap = false)
-    dict = Libcv::Library.hanviet
+  {% for type in {:hanviet, :binh_am} %}
+  def {{type.id}}(input : String, apply_cap = false)
+    dict = Library.{{type.id}}
     Convert.translit(input, dict, apply_cap, true)
   end
 
-  def hanviet(lines : Array(String), apply_cap = false)
-    dict = Libcv::Library.hanviet
+  def {{type.id}}(lines : Array(String), apply_cap = false)
+    dict = Library.{{type.id}}
     lines.map { |line| Convert.translit(line, dict, apply_cap, false) }
   end
+  {% end %}
 
-  def binh_am(input : String, apply_cap = false)
-    dict = Libcv::Library.binh_am
-    Convert.translit(input, dict, apply_cap, true)
+  {% for type in {:title, :plain} %}
+  def cv_{{type.id}}(input : String, dname = "_tonghop")
+    dicts = Library.for_convert(dname)
+    Convert.cv_{{type.id}}(input, *dicts)
   end
 
-  def binh_am(lines : Array(String), apply_cap = false)
-    dict = Libcv::Library.binh_am
-    lines.map { |line| Convert.translit(line, dict, apply_cap, false) }
+  def cv_{{type.id}}(lines : Array(String), dname = "_tonghop")
+    dicts = Library.for_convert(dname)
+    lines.map { |line| Convert.cv_{{type.id}}(line, *dicts) }
   end
+  {% end %}
 
-  def cv_title(input : String, dname = "combine")
-    dicts = Libcv::Library.for_convert(dname)
-    Convert.cv_title(input, *dicts)
-  end
-
-  def cv_title(lines : Array(String), dname = "combine")
-    dicts = Libcv::Library.for_convert(dname)
-    lines.map { |line| Convert.cv_title(line, *dicts) }
-  end
-
-  def cv_plain(input : String, dname = "combine")
-    dicts = Libcv::Library.for_convert(dname)
-    Convert.cv_plain(input, *dicts)
-  end
-
-  def cv_plain(lines : Array(String), dname = "combine")
-    dicts = Libcv::Library.for_convert(dname)
-    lines.map { |line| Convert.cv_plain(line, *dicts) }
-  end
-
-  def cv_mixed(lines : Array(String), dname = "combine")
-    dicts = Libcv::Library.for_convert(dname)
+  def cv_mixed(lines : Array(String), dname = "_tonghop")
+    dicts = Library.for_convert(dname)
     res = [Convert.cv_title(lines.first, *dicts)]
 
     1.upto(lines.size - 1) do |i|
