8328dfe57b1bfe1779fbda8b127ae73a6e3a951c
Nipinium <nipinium@gmail.com>
2020-08-05 04:01:15 +0700

    moving old chap texts to new folder

--- etc/script/backup/assets.sh
+++ etc/script/backup/assets.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-INP=nipin@ssh.nipin.xyz:web/chivi/var/
-OUT=var/
+INP=nipin@ssh.nipin.xyz:web/chivi/var
+OUT=var
 
-rsync -azi "$INP/userdb" $OUT
-rsync -azi "$INP/bookdb/serials" "$OUT/bookdb"
-rsync -azi "$INP/bookdb/indexes" "$OUT/bookdb"
-rsync -azi "$INP/bookdb/chlists" "$OUT/bookdb"
-rsync -azi "$INP/bookdb/chfiles" "$OUT/bookdb"
+rsync -azi "$INP/appcv/members" "$OUT/appcv"
+# rsync -azi "$INP/bookdb/serials" "$OUT/bookdb"
+# rsync -azi "$INP/bookdb/indexes" "$OUT/bookdb"
+# rsync -azi "$INP/bookdb/chlists" "$OUT/bookdb"
+# rsync -azi "$INP/bookdb/chfiles" "$OUT/bookdb"
--- move.rb
+++ move.rb
@@ -1,15 +1,19 @@
+require "json"
 require "fileutils"
 
-INP = "var/appcv/.relics/texts"
+DIR = "var/bookdb/serials"
+INP = "var/bookdb/chfiles"
 OUT = "var/appcv/chtexts"
 
 inps = Dir.glob("#{INP}/*/")
 
 inps.each do |inp_dir|
-  name, seed, sbid = File.basename(inp_dir).split(".")
-  name = name.ljust(8, "0")
+  ubid, seed = File.basename(inp_dir).split(".")
 
-  out_dir = File.join(OUT, "#{name}.#{seed}")
+  info = JSON.parse File.read("#{DIR}/#{ubid}.json")
+  sbid = info["seed_sbids"][seed]
+
+  out_dir = File.join(OUT, "#{ubid}.#{seed}")
   FileUtils.mkdir_p(out_dir)
 
   files = Dir.children(inp_dir)
@@ -20,7 +24,7 @@ inps.each do |inp_dir|
 
     inp_file = File.join(inp_dir, file)
     out_file = File.join(out_dir, "#{sbid}.#{file}")
-
+    # puts [inp_file, out_file]
     FileUtils.mv inp_file, out_file
   end
 end
