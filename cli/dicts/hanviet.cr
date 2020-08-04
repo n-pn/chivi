7609e261434a9485045356cc2060da48be22e14a
Nipinium <nipinium@gmail.com>
2020-08-05 06:01:52 +0700

    remove trichdan words

--- cli/dicts/hanviet.cr
+++ cli/dicts/hanviet.cr
@@ -102,7 +102,7 @@ worker.import_dict!("hanviet/checked-chars.txt", mode: :old_first)
 worker.import_dict!("hanviet/lacviet-chars.txt", mode: :old_first)
 worker.import_dict!("hanviet/lacviet-words.txt", mode: :old_first)
 worker.import_dict!("hanviet/trichdan-chars.txt", mode: :old_first)
-worker.import_dict!("hanviet/trichdan-words.txt", mode: :old_first)
+# worker.import_dict!("hanviet/trichdan-words.txt", mode: :old_first)
 worker.import_dict!("hanviet/verified-chars.txt", mode: :keep_new)
 worker.import_dict!("hanviet/verified-words.txt", mode: :keep_new)
 
