3e8c8be7bb326015aa910830f6e103b5084b5558
Nipinium <nipinium@gmail.com>
2020-09-14 23:27:37 +0700

    fix iCIBA link

--- web/src/routes/_layout/Upsert/Footer.svelte
+++ web/src/routes/_layout/Upsert/Footer.svelte
@@ -2,48 +2,7 @@
   export let key = ''
 </script>
 
-<style lang="scss">
-  footer {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    margin: 0;
-    border-top: 1px solid color(neutral, 3);
-
-    @include bgcolor(neutral, 1);
-    @include radius($sides: bottom);
-  }
-
-  a {
-    // display: inline-block;
-    cursor: pointer;
-    line-height: 1rem;
-    padding: 0.5rem;
-    // border-left: 1px solid color(neutral, 3);
-
-    // max-width: 25vw;
-    // overflow: hidden;
-    // @include truncate();
-
-    @include font-size(2);
-    @include fgcolor(neutral, 7);
-
-    &:hover {
-      background-color: color(neutral, 3);
-    }
-  }
-</style>
-
 <footer>
-  <a
-    class="footer-link"
-    href="https://www.iciba.com/word?w={key}"
-    target="_blank"
-    rel="noopener noreferer">
-    iCIBA
-  </a>
-
   <a
     class="footer-link"
     href="https://translate.google.com/#view=home&op=translate&sl=zh-CN&tl=en&text={key}"
@@ -83,4 +42,45 @@
     rel="noopener noreferer">
     Baidu
   </a>
+
+  <a
+    class="footer-link"
+    href="https://www.iciba.com/{key}"
+    target="_blank"
+    rel="noopener noreferer">
+    iCIBA
+  </a>
 </footer>
+
+<style lang="scss">
+  footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    margin: 0;
+    border-top: 1px solid color(neutral, 3);
+
+    @include bgcolor(neutral, 1);
+    @include radius($sides: bottom);
+  }
+
+  a {
+    // display: inline-block;
+    cursor: pointer;
+    line-height: 1rem;
+    padding: 0.5rem;
+    // border-left: 1px solid color(neutral, 3);
+
+    // max-width: 25vw;
+    // overflow: hidden;
+    // @include truncate();
+
+    @include font-size(2);
+    @include fgcolor(neutral, 7);
+
+    &:hover {
+      background-color: color(neutral, 3);
+    }
+  }
+</style>
