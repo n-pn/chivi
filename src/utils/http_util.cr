3b4df61c7e564938e733201cb38e451c9c1d3a24
Nipinium <nipinium@gmail.com>
2020-09-15 13:06:07 +0700

    add user agent

--- src/utils/http_util.cr
+++ src/utils/http_util.cr
@@ -4,32 +4,36 @@ require "http/client"
 module HttpUtil
   extend self
 
-  LABEL = "http_util"
-  alias SSL = OpenSSL::SSL::Context::Client
-
   UTF8_SITES = {"qu_la", "jx_la", "hetushu", "paoshu8", "zhwenpg"}
 
   def encoding_for(site : String) : String
     UTF8_SITES.includes?(site) ? "UTF-8" : "GBK"
   end
 
+  alias SSL = OpenSSL::SSL::Context::Client
+
   def fetch_html(url : String, encoding = "GBK") : String
-    puts "- <#{LABEL}> GET: [#{url}]".colorize.magenta
+    puts "-- <http_util> GET: [#{url}] --".colorize.magenta
     tls = url.starts_with?("https") ? SSL.insecure : nil
     html = fetch_html(url, encoding, tls, retry: 0)
   end
 
+  HEADERS = HTTP::Headers{
+    "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
+  }
+
   def fetch_html(url : String, encoding : String, tls : SSL?, retry = 0)
-    HTTP::Client.get(url, tls: tls) do |res|
+    HTTP::Client.get(url, tls: tls, headers: HEADERS) do |res|
       res.body_io.set_encoding(encoding, invalid: :skip)
       res.body_io.try(&.gets_to_end) || "404 Not Found!"
     end
   rescue err
-    puts "- <#{LABEL.colorize.red}> error fetching [#{url.colorize.red}]: \
-            {#{err.class}} #{err.colorize.red} (retry: #{retry.colorize.red})"
+    puts "-- <http_util> error fetching [#{url}]: \
+            {#{err.class}} #{err.colorize.red} (retry: #{retry}) --"
 
-    return "500 Server Error!" if retry > 2 # stop trying after 3 times
     retry += 1
+    return "500 Server Error!" if retry > 2 # stop trying after 2 times
+
     sleep 500.milliseconds * retry
     fetch_html(url, encoding, tls, retry)
   end
