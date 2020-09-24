cc62c596d91b78355ed149e174b358e990a0a5bc
Nipinium <nipinium@gmail.com>
2020-09-24 18:05:51 +0700

    remove keyboard hint

--- web/src/routes/_layout.svelte
+++ web/src/routes/_layout.svelte
@@ -97,35 +97,39 @@
     }
   }
 
-  :global([data-kbd]) {
-    position: relative;
-    &:after {
-      display: none;
-      position: absolute;
-      content: attr(data-kbd);
+  // :global([data-kbd]) {
+  //   position: relative;
 
-      top: 0.375rem;
-      right: 0.375rem;
+  //   &:after {
+  //     display: none;
+  //   }
 
-      font-weight: 500;
-      text-transform: uppercase;
-      line-height: 1rem;
-      height: 1.1rem;
+  //   @include hover {
+  //     &:after {
+  //       display: inline-block;
+  //       position: absolute;
+  //       content: attr(data-kbd);
 
-      padding: 0 0.25rem;
+  //       top: 0.375rem;
+  //       right: 0.375rem;
 
-      @include font-size(2);
-      @include fgcolor(neutral, 6);
-      @include bgcolor(yellow, 1);
-      @include border();
-      @include shadow(3);
-      @include radius();
-    }
+  //       font-weight: 500;
+  //       text-transform: uppercase;
+  //       text-align: baseline;
+  //       line-height: 1rem;
+  //       // height: 1rem;
+  //       overflow: hidden;
 
-    @include hover {
-      &:after {
-        display: block;
-      }
-    }
-  }
+  //       margin: 0;
+  //       padding: 0 0.25rem;
+
+  //       @include font-size(2);
+  //       @include fgcolor(neutral, 6);
+  //       @include bgcolor(yellow, 1);
+  //       @include border();
+  //       @include shadow(3);
+  //       @include radius();
+  //     }
+  //   }
+  // }
 </style>
