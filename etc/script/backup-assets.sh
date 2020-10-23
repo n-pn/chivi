2a6bae2de74483bc3f1a5962cf95cc295b37d42b
Nipinium <nipinium@gmail.com>
2020-10-24 00:25:10 +0700

    change file locations

--- etc/script/backup-assets.sh
+++ etc/script/backup-assets.sh
@@ -3,6 +3,6 @@
 REMOTE=nipin@ssh.nipin.xyz:web/chivi
 
 rsync -azi --no-p "$REMOTE/var/appcv/members" "var/appcv"
-rsync -aiz --no-p --ignore-existing "$REMOTE/_db/prime/chtexts" "_db/prime"
+rsync -aiz --no-p --ignore-existing "$REMOTE/_db/prime/chdata/texts" "_db/prime/chdata"
 # rsync -az "$REMOTE/var/appcv/chlists/" "var/appcv/chlists/"
 # rsync -az "$REMOTE/var/appcv/serials/" "var/appcv/serials/"
--- src/kernel/chap_text.cr
+++ src/kernel/chap_text.cr
@@ -4,18 +4,18 @@ require "file_utils"
 class ChapText
   # class methods
 
-  DIR = File.join("_db", "prime", "chtexts")
+  DIR = File.join("_db", "prime", "chdata", "texts")
   FileUtils.mkdir_p(DIR)
 
   SEP_0 = "ǁ"
   SEP_1 = "¦"
 
   CACHE = {} of String => ChapText
-  LIMIT = 3000
+  LIMIT = 2000
 
   def self.load(sname : String, s_bid : String, s_cid : String, mode = 1)
     file = File.join(DIR, sname, s_bid, "#{s_cid}.txt")
-    CACHE.shift if CACHE.size > LIMIT
+    CACHE.clear if CACHE.size > LIMIT
     CACHE[file] ||= new(file, mode)
   end
 
@@ -28,21 +28,7 @@ class ChapText
   def initialize(@file, mode : Int32 = 1)
     return if mode == 0
     return unless mode == 2 || File.exists?(@file)
-
-    dirty = false
-
-    File.each_line(file) do |line|
-      if dirty
-        @zh_data << zh_line(line)
-      elsif line =~ /[\tǁ]/
-        dirty = true
-        @zh_data << zh_line(line)
-      else
-        @zh_data << line
-      end
-    end
-
-    save!(@file) if dirty
+    @zh_data = File.read_lines(file)
   end
 
   def zh_line(line : String)
