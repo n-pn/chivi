6849860978db0119d71bcb6b5d6b906f43b3cddb
Nipinium <nipinium@gmail.com>
2020-12-03 01:06:16 +0700

    move session to config/session

--- src/server.cr
+++ src/server.cr
@@ -4,7 +4,7 @@ Kemal::Session.config do |config|
   config.cookie_name = "chivi"
   config.secret = ENV["SECRET"]? || "chivi-session"
   config.timeout = 3.days
-  config.engine = Kemal::Session::FileEngine.new({:sessions_dir => "tmp/"})
+  config.engine = Kemal::Session::FileEngine.new({:sessions_dir => "config/session"})
   config.gc_interval = 5.minutes
   config.secure = Kemal.config.env == "production"
 end
