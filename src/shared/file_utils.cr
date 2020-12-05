292331de972d8bf4cc345dd99b903a7b33cc8ad9
Nipinium <nipinium@gmail.com>
2020-12-05 22:40:49 +0700

    Add read/write gzip text files

---
+++ src/shared/file_utils.cr
@@ -0,0 +1,34 @@
+require "compress/gzip"
+
+module Chivi::FileUtils
+  extend self
+
+  EXPIRY = Time.utc(2000, 1, 1)
+
+  def read(file : String, expiry = EXPIRY)
+    File.read(file) if recent?(file, expiry)
+  end
+
+  def read_gz(file : String, expiry = EXPIRY)
+    return unless recent?(file, expiry)
+
+    File.open(file) do |io|
+      Compress::Gzip::Reader.open(io, &.gets_to_end)
+    end
+  end
+
+  def save_gz(file : String, input : String)
+    File.open(file, "w") do |io|
+      Compress::Gzip::Writer.open(io, &.print(input))
+    end
+  end
+
+  def recent?(file : String, expiry : Time) : Bool
+    return false unless stat = File.info?(file)
+    stat.modification_time >= expiry
+  end
+end
+
+# file = ".tmp/test.txt.gz"
+# Chivi::FileUtils.save_gz(file, "test test test!")
+# puts Chivi::FileUtils.read_gz(file)
