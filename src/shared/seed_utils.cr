3c11b85efc94f2a53ef031809dca238e05d6e234
Nipinium <nipinium@gmail.com>
2020-12-05 23:50:31 +0700

    merge chap_utils to seed_utils

--- src/shared/seed_utils.cr
+++ src/shared/seed_utils.cr
@@ -116,6 +116,48 @@ module Chivi::SeedUtils
     res << acc unless acc.empty?
     res
   end
+
+  NUMS = "零〇一二两三四五六七八九十百千"
+  TAGS = "章节幕回折"
+  SEPS = ".，,、：: "
+
+  FIX_RE_0 = /^(第[#{NUMS}\d]+[集卷].*?)(第?[#{NUMS}\d]+[#{TAGS}].*)$/
+  FIX_RE_1 = /^(第[#{NUMS}\d]+[集卷].*?)(（\p{N}+）.*)$/
+  FIX_RE_2 = /^【?(第[#{NUMS}\d]+[集卷])】?\s*(.+)$/
+
+  def fix_title(title : String, label = "正文")
+    if match = FIX_RE_0.match(title) || FIX_RE_1.match(title) || FIX_RE_2.match(title)
+      _, label, title = match
+      label = fix_spaces(label)
+    end
+
+    "#{label}  #{format_title(title)}"
+  end
+
+  FORMAT_RE_0 = /^第?([#{NUMS}\d]+)([#{TAGS}])[#{SEPS}]*(.*)$/
+  FORMAT_RE_1 = /^\d+\.\s*第(.+)([#{TAGS}])[#{SEPS}]*(.+)/ # 69shu 1
+  FORMAT_RE_2 = /^第(.+)([#{TAGS}])\s\d+\.\s*(.+)/         # 69shu 2
+  FORMAT_RE_3 = /^([#{NUMS}\d]+)[#{SEPS}]+(.*)$/
+  FORMAT_RE_4 = /^\（(\p{N}+)\）[#{SEPS}]*(.*)$/
+
+  def format_title(title : String)
+    if match = FORMAT_RE_0.match(title)
+      _, idx, tag, title = match
+      return "第#{idx}#{tag} #{fix_spaces(title)}"
+    end
+
+    if match = FORMAT_RE_1.match(title) || FORMAT_RE_2.match(title)
+      _, idx, tag, title = match
+      return "第#{idx}#{tag} #{title}"
+    end
+
+    if match = FORMAT_RE_3.match(title) || FORMAT_RE_4.match(title)
+      _, idx, title = match
+      return "#{idx}. #{fix_spaces(title)}"
+    end
+
+    fix_spaces(title)
+  end
 end
 
 # puts Chivi::SeedUtils.parse_time("5/14/2020 7:00:48 AM")
