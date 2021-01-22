2cc8bf108dc309f8acc34bda3df630e2bfb13a00
Nipinium <nipinium@gmail.com>
2021-01-22 18:13:36 +0700

    untrack web/public folder

--- tasks/_remote/filedb-upload.sh
+++ tasks/_remote/filedb-upload.sh
@@ -14,7 +14,8 @@ rsync -azi --no-p "_db/nvdata/nvinfos" "$SSH/_db/nvdata"
 rsync -azi --no-p "_db/chdata/chinfos" "$SSH/_db/chdata"
 
 ## upload book covers
-rsync -azi --no-p "_db/bcover/" "$SSH/web/public/covers/"
+rsync -azi --no-p "web/public" "$SSH/web"
+# rsync -azi --no-p "_db/bcover/" "$SSH/web/public/covers/"
 
 ## upload user data
 rsync -azi --no-p "_db/userdb/viusers" "$SSH/_db/userdb"
