a4b21ea4d7ff21fda5b47a2b71eb84fbc6c9f7f9
Nipinium <nipinium@gmail.com>
2021-01-23 00:46:24 +0700

    ditto

--- src/_utils/http_utils.cr
+++ src/_utils/http_utils.cr
@@ -8,15 +8,14 @@ module CV::HttpUtils
 
   def get_html(url : String, encoding : String) : String
     cmd = "curl -L -k -s -m 30 '#{url}'"
-    cmd += " | iconv -f #{encoding} -t UTF-8" if encoding != "UTF-8"
+    cmd += " | iconv -c -f #{encoding} -t UTF-8" if encoding != "UTF-8"
 
     try = 0
 
     loop do
       puts "[GET: <#{url}> (try: #{try})]".colorize.magenta
-
       html = `#{cmd}`.strip
-      return html.sub(/\b#{encoding}\b/i, "utf-8") unless html.empty?
+      return fix_charset(html, encoding) if html.size > 10
     rescue err
       puts err.colorize.red
     ensure
@@ -24,6 +23,11 @@ module CV::HttpUtils
       sleep 500.milliseconds * try
       raise "500 Server Error!" if try > 3
     end
+  end
+
+  private def fix_charset(html : String, encoding : String)
+    return html if encoding == "UTF-8"
+    html.sub(/charset=#{encoding}/i, "charset=utf-8")
   end
 
   def encoding_for(s_name : String) : String
