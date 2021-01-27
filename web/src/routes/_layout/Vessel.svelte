65922101ff2be55833a2636bfe07b4faa4e65920
Nipinium <nipinium@gmail.com>
2021-01-28 00:00:21 +0700

    move Header back to Vessel

--- web/src/routes/_layout/Vessel.svelte
+++ web/src/routes/_layout/Vessel.svelte
@@ -1,17 +1,39 @@
 <script>
-  import { l_scroll } from '$src/stores'
+  import { u_dname, u_power, l_scroll } from '$src/stores'
+  import SIcon from '$blocks/SIcon'
 
-  import Header from '$layout/Header'
+  import Signin from '$widget/Signin'
+  import Usercp from '$widget/Usercp'
 
   export let shift = false
+  let active_usercp = false
 </script>
 
-<main class="main" class:_shift={shift}>
-  <Header>
-    <slot name="header-left" slot="header-left" />
-    <slot name="header-right" slot="header-right" />
-  </Header>
+<header class="app-header" class:_shift={shift} class:_clear={$l_scroll > 0}>
+  <nav class="center -wrap">
+    <div class="-left">
+      <a href="/" class="header-item _brand">
+        <img src="/chivi-logo.svg" alt="logo" />
+        <span class="header-text _show-md">Chivi</span>
+      </a>
 
+      <slot name="header-left" />
+    </div>
+
+    <div class="-right">
+      <slot name="header-right" />
+
+      <button class="header-item" on:click={() => (active_usercp = true)}>
+        <SIcon name="user" />
+        <span class="header-text _show-md">
+          {#if $u_power > 0}{$u_dname} [{$u_power}]{:else}Khách{/if}
+        </span>
+      </button>
+    </div>
+  </nav>
+</header>
+
+<main class="main" class:_shift={shift}>
   <div class="center">
     <slot />
   </div>
@@ -20,6 +42,14 @@
     <slot name="footer" />
   </footer>
 </main>
+
+{#if active_usercp}
+  {#if $u_dname == 'Khách'}
+    <Signin bind:actived={active_usercp} />
+  {:else}
+    <Usercp bind:actived={active_usercp} />
+  {/if}
+{/if}
 
 <style lang="scss">
   $page-width: 54rem;
@@ -39,13 +69,13 @@
     position: relative;
     // margin-bottom: $footer-height;
 
-    &._shift {
-      @include props(padding-right, $lg: 30rem);
+    .center {
+      min-height: calc(100vh - 3rem);
     }
   }
 
-  .center {
-    min-height: calc(100vh - 3rem);
+  ._shift {
+    @include props(padding-right, $lg: 30rem);
   }
 
   .footer {
