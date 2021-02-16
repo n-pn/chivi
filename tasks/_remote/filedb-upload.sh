e5f70a4b014991b5071dfcbb4df5d7b509df987f
Nipinium <nipinium@gmail.com>
2021-02-17 02:58:59 +0700

    cleanup

--- tasks/_remote/filedb-upload.sh
+++ tasks/_remote/filedb-upload.sh
@@ -10,7 +10,10 @@ rsync -azi "_db/dictdb/active/" "$SSH/_db/dictdb/remote/"
 rsync -azi --no-p "_db/nvdata/nvinfos" "$SSH/_db/nvdata"
 
 ## upload chap data
-rsync -azi --no-p "_db/chdata/chinfos" "$SSH/_db/chdata"
+rsync -azi --no-p "_db/chdata/chorigs" "$SSH/_db/chdata"
+rsync -azi --no-p "_db/chdata/chheads" "$SSH/_db/chdata"
+# rsync -azi --no-p "_db/chdata/chinfos" "$SSH/_db/chdata"
+# rsync -azi --no-p "_db/chdata/chstats" "$SSH/_db/chdata"
 
 ## upload book covers
 rsync -azi --no-p --exclude "covers" "web/public" "$SSH/web"
