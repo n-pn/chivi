5ccca8dbdabf25a1663648d8c13ae1f63675a026
Nipinium <nipinium@gmail.com>
2021-03-10 04:37:59 +0700

    cleanup upload_zhtext script

--- tasks/upload_zhtext.cr
+++ tasks/upload_zhtext.cr
@@ -3,25 +3,25 @@
 # require "json"
 require "colorize"
 
-INP = "_db/chdata/zhtexts"
 SSH = "nipin@ssh.chivi.xyz"
-OUT = "#{SSH}:www/chivi.xyz/#{INP}"
+INP = "_db/chdata/zhtexts"
+OUT = "/home/nipin/www/chivi.xyz"
 
 def upload_texts(sname : String)
   seed_dir = File.join(INP, sname)
-  `ssh #{SSH} mkdir -p /home/nipin/www/chivi.xyz/#{seed_dir}`
+  remote_dir = "#{OUT}/#{seed_dir}"
+  puts `ssh #{SSH} mkdir -p "#{remote_dir}"`
 
   dirs = Dir.children(seed_dir).sort_by { |x| x.to_i? || 0 }
-
   dirs.each_with_index(1) do |snvid, idx|
     text_dir = File.join(seed_dir, snvid)
     puts "- <#{idx}/#{dirs.size}> [#{text_dir}]".colorize.cyan
-    puts `rsync -aiWS --inplace --no-p "#{text_dir}" "#{OUT}/#{sname}"`.colorize.yellow
+    puts `rsync -aiWS --inplace --no-p "#{text_dir}" "#{SSH}:#{remote_dir}"`.colorize.yellow
   end
 end
 
 existed = Dir.children(INP)
 snames = ARGV.empty? ? existed : ARGV.select { |x| existed.includes?(x) }
 
-puts "INPUT: #{snames}".colorize.yellow.bold
+puts "-- INPUT: #{snames} --".colorize.yellow.bold
 snames.each { |sname| upload_texts(sname) }
