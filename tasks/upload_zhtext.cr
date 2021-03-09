5ff24fbd9321dfc7a9f4f2cd235e0dd1f4751806
Nipinium <nipinium@gmail.com>
2021-03-10 04:27:38 +0700

    improve upload zhtexts script

--- tasks/upload_zhtext.cr
+++ tasks/upload_zhtext.cr
@@ -8,21 +8,15 @@ SSH = "nipin@ssh.chivi.xyz"
 OUT = "#{SSH}:www/chivi.xyz/#{INP}"
 
 def upload_texts(sname : String)
-  dirs = Dir.glob(File.join(INP, sname, "*/"))
-  dirs.sort_by! { |x| File.basename(x).to_i? || 0 }
+  seed_dir = File.join(INP, sname)
+  `ssh #{SSH} mkdir -p /home/nipin/www/chivi.xyz/#{seed_dir}`
 
-  dirs.each_with_index(1) do |txt_dir, i|
-    snvid = File.basename(txt_dir)
-    `ssh #{SSH} mkdir -p /home/nipin/www/chivi.xyz/#{INP}/#{sname}/#{snvid}`
+  dirs = Dir.children(seed_dir).sort_by { |x| x.to_i? || 0 }
 
-    files = Dir.glob(File.join(txt_dir, "*.zip"))
-    puts "- <#{i}/#{dirs.size}> [#{sname}/#{snvid}]: #{files.size} files".colorize.cyan
-
-    files.each_with_index(1) do |file, idx|
-      puts "-- <#{idx}/#{files.size}> [#{file}]".colorize.blue
-      res = `rsync -ai --no-p "#{file}" "#{OUT}/#{sname}/#{snvid}"`
-      puts res.empty? ? "  existed, skipping!".colorize.dark_gray : res.colorize.yellow
-    end
+  dirs.each_with_index(1) do |snvid, idx|
+    text_dir = File.join(seed_dir, snvid)
+    puts "- <#{idx}/#{dirs.size}> [#{text_dir}]".colorize.cyan
+    puts `rsync -aiWS --inplace --no-p "#{text_dir}" "#{OUT}/#{sname}"`.colorize.yellow
   end
 end
 
