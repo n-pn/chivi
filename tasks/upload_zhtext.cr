00fb15dc1bae1db5cea70e6373423af888828475
Nipinium <nipinium@gmail.com>
2021-03-10 05:05:51 +0700

    final improvement for zhtext upload

--- tasks/upload_zhtext.cr
+++ tasks/upload_zhtext.cr
@@ -13,11 +13,23 @@ def upload_texts(sname : String)
   puts `ssh #{SSH} mkdir -p "#{remote_dir}"`
 
   dirs = Dir.children(seed_dir).sort_by { |x| x.to_i? || 0 }
+
+  threads = dirs.size
+  threads = 8 if threads > 8
+  channel = Channel(Nil).new(threads + 1)
+
   dirs.each_with_index(1) do |snvid, idx|
-    text_dir = File.join(seed_dir, snvid)
-    puts "- <#{idx}/#{dirs.size}> [#{text_dir}]".colorize.cyan
-    puts `rsync -aiWS --inplace --no-p "#{text_dir}" "#{SSH}:#{remote_dir}"`.colorize.yellow
+    channel.receive if idx > threads
+
+    spawn do
+      text_dir = File.join(seed_dir, snvid)
+      puts "- <#{idx}/#{dirs.size}> [#{text_dir}]".colorize.cyan
+      puts `rsync -aiWS --inplace --no-p -e "ssh -T -c aes128-ctr -o Compression=no -x" "#{text_dir}" "#{SSH}:#{remote_dir}"`.colorize.yellow
+      channel.send(nil)
+    end
   end
+
+  threads.times { channel.receive }
 end
 
 existed = Dir.children(INP)
