faf28ab4568f9c0e74a2a795553e83003b9a3661
Nipinium <nipinium@gmail.com>
2021-06-17 21:57:11 +0700

    copy tabkv from old code

---
+++ src/tabkv/order_map.cr
@@ -0,0 +1,204 @@
+require "./value_map"
+
+class CV::OrderMap < CV::ValueMap
+  @@klass = "order_map"
+
+  class Node
+    getter key : String
+    property val : Int32
+    property prev : Node?
+    property succ : Node?
+
+    def initialize(@key, @val = -1, @prev = nil, @succ = nil)
+    end
+
+    def set_prev(node : self) : Nil
+      if prev = @prev
+        prev.succ = node
+        node.prev = prev
+      end
+
+      node.succ = self
+      @prev = node
+    end
+
+    def set_succ(node : self) : Nil
+      if succ = @succ
+        succ.prev = node
+        node.succ = succ
+      end
+
+      node.prev = self
+      @succ = node
+    end
+
+    def set_far_prev(node : self) : Nil
+      prev = self
+
+      while prev = prev.prev
+        if prev.val < node.val
+          return prev.set_succ(node)
+        end
+      end
+    end
+
+    def set_far_succ(node : self) : Nil
+      succ = self
+
+      while succ = succ.succ
+        if succ.val > node.val
+          return succ.set_prev(node)
+        end
+      end
+    end
+
+    def unlink!
+      @prev.try(&.succ = @succ)
+      @succ.try(&.prev = @prev)
+    end
+  end
+
+  class PrevIter
+    getter curr
+
+    def initialize(@curr : Node, @done : Node)
+    end
+
+    def next
+      if @curr != @done
+        @curr = @curr.prev.not_nil!
+      else
+        nil
+      end
+    end
+  end
+
+  class SuccIter
+    getter curr
+
+    def initialize(@curr : Node, @done : Node)
+    end
+
+    def next
+      if @curr != @done
+        @curr = @curr.succ.not_nil!
+      else
+        nil
+      end
+    end
+  end
+
+  class List
+    getter data : Hash(String, Node)
+    getter head : Node
+    getter tail : Node
+
+    def initialize
+      @data = {} of String => Node
+
+      @head = Node.new("_head_", Int32::MIN)
+      @tail = Node.new("_tail_", Int32::MAX)
+      @head.set_succ(@tail)
+
+      @_min = 0
+      @_max = 0
+      @_avg = 0
+    end
+
+    def set(key : String, val : Int32) : Nil
+      if old_node = @data[key]?
+        return if old_node.val == val
+
+        old_node.val = val
+        node = old_node
+        node.unlink!
+      else
+        node = @data[key] = Node.new(key, val)
+      end
+
+      # fix order
+      if @data.size == 1
+        @_max = val
+        @_min = val
+        @tail.set_prev(node)
+      elsif val >= @_max
+        @_max = val
+        @tail.set_prev(node)
+      elsif val <= @_min
+        @_min = val
+        @head.set_succ(node)
+      elsif @_max &- val < val &- @_min
+        @tail.set_far_prev(node)
+      else
+        @head.set_far_succ(node)
+      end
+    end
+
+    def delete(key : String) : Nil
+      @data[key]?.try(&.unlink!)
+    end
+
+    def each(node : Node = @head)
+      SuccIter.new(node.not_nil!, @tail)
+    end
+
+    def reverse_each(node : Node = @tail)
+      PrevIter.new(node, @head)
+    end
+
+    def each(node = @head.succ) : Nil
+      while node
+        break if node == @tail
+        yield node
+        node = node.succ
+      end
+    end
+
+    def reverse_each(node = @tail.prev) : Nil
+      while node
+        break if node == @head
+        yield node
+        node = node.prev
+      end
+    end
+  end
+
+  getter _idx = List.new
+
+  def set(key : String, val : Int32, ext = [] of String) : Bool
+    return false unless super(key, [val.to_s].join(ext))
+    @_idx.set(key, val)
+    true
+  end
+
+  def set(key : String, vals : Array(String)) : Bool
+    return false unless super(key, vals)
+
+    if val = vals[0]?.try(&.to_i?)
+      @_idx.set(key, val)
+    else
+      @_idx.delete(key)
+    end
+
+    true
+  end
+
+  def delete(key : String)
+    @_idx.delete(key)
+    super
+  end
+
+  def get_idx(key : String) : Node?
+    @_idx.data[key]?
+  end
+
+  def get_val(key : String) : Int32
+    get_idx(key).try(&.val) || -1
+  end
+
+  def each
+    @_idx.each do |node|
+      yield(node.key, @data[node.key])
+    end
+  end
+end
---
+++ src/tabkv/zip_store.cr
@@ -0,0 +1,109 @@
+require "compress/zip"
+
+class CV::ZipStore
+  def self.read(file : String)
+    dir_path = File.dirname(file)
+    zip_file = "#{dir_path}.zip"
+
+    new(zip_file, dir_path).read(File.basename(file))
+  end
+
+  def self.read(zip_file : String, entry_name : String)
+    new(zip_file).read(entry_name)
+  end
+
+  getter zip_file : String
+  getter root_dir : String
+
+  def initialize(@zip_file, @root_dir = zip_file.sub(/.zip$/, ""))
+  end
+
+  def entries(min_size : Int32 = 1)
+    zip_entries.concat(dir_entries).uniq!
+  end
+
+  def zip_entries(min_size : Int32 = 1)
+    open_zip do |zip|
+      # skip files if file size smaller than min_size
+      zip.entries.reject { |x| x.uncompressed_size < min_size }.map(&.filename)
+    end || [] of String
+  end
+
+  def dir_entries(min_size : Int32 = 1)
+    return [] of String unless File.directory?(@root_dir)
+
+    Dir.children(@root_dir).reject do |fname|
+      fpath = root_path(fname)
+      # skip files if file size smaller than min_size
+      File.directory?(fpath) || File.size(fpath) < min_size
+    end
+  end
+
+  def contains?(fname : String) : Bool
+    File.exists?(root_path(fname)) || archives?(fname)
+  end
+
+  def archives?(fname : String) : Bool
+    open_zip { |zip| return true if zip[fname]? } || false
+  end
+
+  def read(fname : String)
+    fpath = root_path(fname)
+    return File.read(fpath) if File.exists?(fpath)
+    open_zip(&.[fname]?.try(&.open(&.gets_to_end)))
+  end
+
+  def mtime(fname : String)
+    File.info?(root_path(fname)).try { |x| return x.modification_time }
+    zip_entry(fname, &.try(&.time))
+  end
+
+  def add_file!(fname : String, mode : Symbol = :update) : Nil
+    fpath = root_path(fname)
+    unless File.exists?(fpath)
+      return puts "[File <#{fname}> not found, skip zipping!]".colorize.dark_gray
+    end
+
+    flags = zip_flags(mode)
+    puts `zip #{flags} #{@zip_file} #{fpath}`
+    puts "- [Added file <#{fname}> to zip: <#{@zip_file}>]\n".colorize.yellow
+  end
+
+  def compress!(mode : Symbol = :update, glob = "*.*") : Nil
+    return puts "[Nothing to compress!]".colorize.dark_gray if Dir.empty?(root_dir)
+
+    flags = zip_flags(mode)
+    files = Dir.glob("#{root_dir}/#{glob}")
+
+    puts `zip #{flags} #{@zip_file} #{root_dir}/#{glob}`
+    puts "- [Added #{files.size} files to zip: <#{@zip_file}>]\n".colorize.yellow
+  end
+
+  def zip_flags(mode : Symbol)
+    # zip flags:
+    # `-j` means remove folder root
+    # `-q` means quiet output mode
+
+    case mode
+    when :archive
+      "-jqm" # move file to zip
+    when :freshen
+      "-jqfm" # only move existed (in zip) files
+    else
+      "-jqu" # update if newer
+    end
+  end
+
+  def root_path(fname : String)
+    File.join(@root_dir, fname)
+  end
+
+  private def open_zip
+    return unless File.exists?(@zip_file)
+    Compress::Zip::File.open(@zip_file) { |zip| yield zip }
+  end
+
+  private def zip_entry(fname : String)
+    open_zip { |zip| yield zip[fname]? }
+  end
+end
