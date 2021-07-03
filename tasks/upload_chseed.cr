d1564eb01c267aa048c23aa7345dfb7dbe95dee9
Nipinium <nipinium@gmail.com>
2021-07-04 02:10:11 +0700

    fix upload script

--- tasks/upload_chseed.cr
+++ tasks/upload_chseed.cr
@@ -9,59 +9,55 @@ module CV::UploadSeed
   INP = "_db/chseed"
   OUT = "/home/nipin/srv/chivi.xyz"
 
-  def upload_all!(snames : Array(String))
-    puts "-- INPUT: #{snames} --".colorize.yellow.bold
+  def run!(argv = ARGV)
+    exists = Dir.children(INP)
 
-    channel = Channel(Nil).new(snames.size)
+    snames = ARGV.select { |x| exists.includes?(x) }
+    snames = exists if snames.empty?
 
-    if ARGV.includes?("-prod")
-      ssh = "nipin@ssh.chivi.xyz"
-    else
-      ssh = "nipin@dev.chivi.xyz"
-    end
-
-    snames.each do |sname|
-      spawn do
-        upload!(sname, ssh)
-      ensure
-        channel.send(nil)
-      end
-    end
-
-    snames.size.times { channel.receive }
+    host = upload_host(argv)
+    snames.each { |sname| upload!(sname, host) }
   end
 
-  def upload!(sname : String, ssh = "nipin@ssh.chivi.xyz")
+  private def upload_host(argv = ARGV)
+    case argv
+    when .includes?("-p"), .includes?("--prod")
+      "nipin@ssh.chivi.xyz"
+    else
+      "nipin@dev.chivi.xyz"
+    end
+  end
+
+  def upload!(sname : String, host = "nipin@ssh.chivi.xyz")
+    puts "upload to: #{host.colorize.blue}, sname: #{sname.colorize.blue}"
+
     target_dir = File.join(INP, sname)
     remote_dir = "#{OUT}/#{target_dir}"
 
-    puts `ssh #{ssh} mkdir -p "#{remote_dir}"`
+    `ssh #{host} mkdir -p "#{remote_dir}"`
+    return unless $?.success?
 
     dirs = Dir.children(target_dir).sort_by { |x| x.to_i? || 0 }
 
-    threads = dirs.size
-    threads = 8 if threads > 8
-    channel = Channel(Nil).new(threads + 1)
+    wkrs = dirs.size
+    wkrs = 8 if wkrs > 8
+    chan = Channel(Nil).new(wkrs + 1)
 
-    command = %{rsync -aiWS --inplace --no-p -e "ssh -T -c aes128-ctr -o Compression=no -x"}
+    cmd = %{rsync -aiWS --inplace --no-p -e "ssh -T -c aes128-ctr -o Compression=no -x"}
 
     dirs.each_with_index(1) do |snvid, idx|
-      channel.receive if idx > threads
+      chan.receive if idx > wkrs
 
       spawn do
         text_dir = File.join(target_dir, snvid)
         puts "- <#{idx}/#{dirs.size}> [#{text_dir}]".colorize.cyan
-        puts `#{command} "#{text_dir}" "#{ssh}:#{remote_dir}"`.colorize.yellow
-        channel.send(nil)
+        puts `#{cmd} "#{text_dir}" "#{host}:#{remote_dir}"`.colorize.yellow
+        chan.send(nil)
       end
     end
 
-    threads.times { channel.receive }
+    wkrs.times { chan.receive }
   end
 end
 
-existed = Dir.children(CV::UploadSeed::INP)
-uploads = ARGV.select { |x| existed.includes?(x) }
-uploads = existed if uploads.empty?
-
-CV::UploadSeed.upload_all!(uploads)
+CV::UploadSeed.run!
