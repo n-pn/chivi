9993a4f8d5447a87f5213fa3c3ef8b0cc4e33fd5
Nipinium <nipinium@gmail.com>
2021-07-05 22:08:08 +0700

    make python script run on whole folder to prevent loading pkuseg too many time

--- tasks/postag/invoke-pkuseg.cr
+++ tasks/postag/invoke-pkuseg.cr
@@ -12,29 +12,27 @@ class CV::Postag
     @out_dir = File.join(OUT_DIR, @sname, @snvid)
   end
 
-  def init!(wrks = 3)
-    files = extract_txt(tag_mode: 1)
-    postag_files(files, wrks: wrks)
+  SCRIPT = "tasks/postag/pkuseg-runner.py"
+  IOPIPE = Process::Redirect::Inherit
+
+  def run!(wrks = 3)
+    extract_txt(tag_mode: 1)
+    Process.run("python3", [SCRIPT, @sname, @snvid], input: IOPIPE, output: IOPIPE)
   end
 
-  def extract_txt(tag_mode = 1) : Array(String)
-    zip_files = Dir.glob("#{@inp_dir}/*.zip")
-    tag_mode = 2 if zip_files.size < 3 # parse all if total chapters < 200
+  def extract_txt(tag_mode = 1) : Nil
+    files = Dir.glob("#{@inp_dir}/*.zip")
+    tag_mode = 2 if files.size < 3 # parse all if total chapters < 200
 
     FileUtils.mkdir_p(@out_dir)
-    output = [] of String
 
-    zip_files.each do |zip_file|
-      output.concat extract_zip(zip_file, tag_mode: tag_mode)
+    files.each do |zip_file|
+      extract_zip(zip_file, tag_mode: tag_mode)
       tag_mode = 0 if tag_mode == 1 # reset tag_mode unless tagging all
     end
-
-    output
   end
 
-  def extract_zip(zip_file : String, tag_mode = 0) : Array(String)
-    res = [] of String
-
+  def extract_zip(zip_file : String, tag_mode = 0) : Nil
     Compress::Zip::File.open(zip_file) do |file|
       file.entries.each_with_index do |entry, index|
         next unless should_tag?(tag_mode, index)
@@ -44,11 +42,8 @@ class CV::Postag
         next if !@redo && File.exists?(tag_file)
 
         entry.open { |io| File.write(txt_file, io.gets_to_end) }
-        res << txt_file
       end
     end
-
-    res
   end
 
   # tag_mode:
@@ -59,40 +54,6 @@ class CV::Postag
     return true if tag_mode == 2
     return true if chap_index % 10 == 0
     tag_mode == 1 && chap_index < 50
-  end
-
-  def postag_files(files : Array(String), wrks = 3)
-    return if files.empty?
-    puts "[ To be tagged: #{files.size} files ]".colorize.yellow
-
-    wrks = files.size if wrks > files.size
-    chan = Channel(Nil).new(wrks)
-
-    files.each_with_index(1) do |file, idx|
-      spawn do
-        add_postag(file, label: "#{idx}/#{files.size}")
-      rescue err
-        puts err.message.colorize.red
-      ensure
-        chan.send(nil)
-      end
-
-      chan.receive if idx > wrks
-    end
-
-    wrks.times { chan.receive }
-  end
-
-  def add_postag(txt_file : String, label = "1/1") : Nil
-    raise "File #{txt_file} not found!" unless File.exists?(txt_file)
-    tag_file = txt_file.sub(".txt", ".dat")
-
-    cap = "#{@sname}/#{@snvid}/#{File.basename(txt_file)}"
-    res = `python3 tasks/postag/pkuseg-runner.py "#{txt_file}" "#{tag_file}"`
-    puts "- <#{label}> [#{cap}] #{res.sub("\t", "  ")}".colorize.blue
-
-    raise "Output #{tag_file} not found!" unless $?.success? && File.exists?(tag_file)
-    File.delete(txt_file)
   end
 end
 
@@ -105,14 +66,23 @@ OptionParser.parse(ARGV) do |opt|
   opt.on("-r", "Redo postagging") { redo = true }
 end
 
-lines = File.read_lines("priv/zhseed.tsv")
+# wrks = files.size if wrks > files.size
+chan = Channel(Nil).new(wrks)
+
+lines = File.read_lines("priv/zhseed.tsv").reject(&.empty?)
 lines.each_with_index(1) do |line, idx|
-  parts = line.split('\t')
-  next if parts.size < 4
+  spawn do
+    sname, snvid, _, bslug = line.split('\t')
+    puts "- <#{idx}/#{lines.size}> [#{sname}/#{snvid}] #{bslug}".colorize.yellow
+    tagger = CV::Postag.new(sname, snvid, redo: redo)
+    tagger.run!
+  rescue err
+    puts err.message.colorize.red
+  ensure
+    chan.send(nil)
+  end
 
-  sname, snvid, _, bslug = parts
-  puts "- <#{idx}/#{lines.size}> [#{sname}/#{snvid}] #{bslug}"
-
-  tagger = CV::Postag.new(sname, snvid, redo: redo)
-  tagger.init!
+  chan.receive if idx > wrks
 end
+
+wrks.times { chan.receive }
--- tasks/postag/pkuseg-runner.py
+++ tasks/postag/pkuseg-runner.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
-import sys
-import pkuseg
+import os, sys, glob, pkuseg
 
-argv_len = len(sys.argv)
+DIR = "_db/vpinit/pkuseg"
 
-inp_file = sys.argv[1] if argv_len > 1 else None
-out_file = sys.argv[2] if argv_len > 2 else None
-n_thread = sys.argv[3] if argv_len > 3 else 10
+def tag_book(sname, snvid, nthread = 10):
+  nvdir = os.path.join(DIR, sname, snvid)
+  files = glob.glob(os.path.join(nvdir, "*.txt"))
 
-if inp_file and out_file:
-  pkuseg.test(inp_file, out_file, postag = True, nthread = int(n_thread))
+  count = len(files)
+  index = 0
+  label = "[" + sname + "/" + snvid + "] <{}/" + str(count) + "> {}"
+
+  for inp_file in files:
+    out_file = inp_file.replace('.txt', '.dat')
+
+    fname = os.path.basename(inp_file)
+    index += 1
+
+    print(label.format(index, fname), end='')
+    pkuseg.test(inp_file, out_file, postag = True, nthread = nthread)
+
+    os.remove(inp_file)
+
+asize = len(sys.argv)
+sname = sys.argv[1] if asize > 1 else None
+snvid = sys.argv[2] if asize > 2 else None
+nthread = int(sys.argv[3]) if asize > 3 else 10
+
+if sname and snvid:
+  tag_book(sname, snvid, nthread)
