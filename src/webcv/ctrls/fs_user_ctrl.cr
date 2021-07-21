bd40fb67b8fb3b98ad954fddc3b8712b0a3b2ae1
Nipinium <nipinium@gmail.com>
2021-07-21 07:05:01 +0700

    fix persisting user settings

--- src/webcv/ctrls/fs_user_ctrl.cr
+++ src/webcv/ctrls/fs_user_ctrl.cr
@@ -7,7 +7,6 @@ class CV::FsUserCtrl < CV::BaseCtrl
 
   def logout
     session.delete("cu_uname")
-    save_session!
     return_user
   end
 
@@ -49,24 +48,29 @@ class CV::FsUserCtrl < CV::BaseCtrl
 
       ViUser.wtheme.set!(cu_uname, wtheme)
       ViUser.tlmode.set!(cu_uname, tlmode)
+
+      session["cu_wtheme"] = wtheme
+      session["cu_tlmode"] = tlmode
+
       ViUser.save!
     end
 
     return_user
   end
 
-  private def sigin_user!(dname : String)
+  private def sigin_user!(dname : String, uname = dname.downcase,
+                          power = ViUser.get_power(uname))
     session["cu_uname"] = dname
-    save_session!
+    session["cu_privi"] = power
+    session["cu_wtheme"] = ViUser.get_wtheme(uname)
+    session["cu_tlmode"] = ViUser.get_tlmode(uname)
   end
 
-  private def return_user
-    render_json({
-      uname: cu_dname,
-      privi: cu_privi,
-
-      wtheme: ViUser.get_wtheme(cu_uname),
-      tlmode: ViUser.get_tlmode(cu_uname),
-    })
+  private def return_user(uname = cu_dname,
+                          privi = cu_privi,
+                          wtheme = cu_wtheme,
+                          tlmode = cu_tlmode)
+    save_session!
+    render_json({uname: uname, privi: privi, wtheme: wtheme, tlmode: tlmode})
   end
 end
