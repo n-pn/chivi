9e732617a2ff3c62150df2311cc1f6e1368c925d
Nipinium <nipinium@gmail.com>
2021-10-02 15:31:53 +0700

    cleanup apply cap functionality

--- src/libcv/mt_core/mt_node/apply_cap.cr
+++ src/libcv/mt_core/mt_node/apply_cap.cr
@@ -3,36 +3,34 @@ require "../../../cutil/text_utils"
 module MTL::ApplyCap
   def apply_cap!(cap : Bool = true) : self
     node = self
-    node.capitalize! if cap
 
-    while node = node.succ?
-      case node.tag
-      when .none?
-        next
-      when .puncts?
-        cap = node.should_cap?(cap)
-      else
-        node.capitalize! if cap
-        cap = false
-      end
+    while node
+      cap = node.capitalize!(cap)
+      node = node.succ?
     end
 
     self
   end
 
-  def capitalize! : Nil
-    return if @tag.urlstr? || @tag.artstr? || @tag.none?
-    @val = CV::TextUtils.capitalize(@val)
+  def capitalize!(cap : Bool = false) : Bool
+    case @tag
+    when .none?   then cap
+    when .puncts? then cap_after_punct?(cap) # TODO: merge this with should_cap?
+    when .urlstr? then false
+    when .artstr? then false
+    else
+      @val = CV::TextUtils.capitalize(@val) if cap
+      false
+    end
   end
 
-  def should_cap?(prev = false) : Bool
+  private def cap_after_punct?(prev = false) : Bool
     case @tag
     when .quoteop?, .exmark?, .qsmark?,
          .pstop?, .colon?, .middot?, .titleop?
       true
     when .brackop? then @val[0] == '(' ? prev : true
-    when .puncts?  then prev
-    else                false
+    else                prev
     end
   end
 end
