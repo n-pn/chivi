2b03ed11c639f5d578fb2548fdbfab0938c3d64d
Nipinium <nipinium@gmail.com>
2021-10-08 06:05:08 +0700

    proper add cap for list

--- src/libcv/mt_core/mt_node/apply_cap.cr
+++ src/libcv/mt_core/mt_node/apply_cap.cr
@@ -31,7 +31,7 @@ module MTL::ApplyCap
     when .quoteop?, .exmark?, .qsmark?,
          .pstop?, .colon?, .middot?, .titleop?
       true
-    when .pdeci?   then @prev.try(&.numlat?) || prev
+    when .pdeci?   then @prev.try { |x| x.numlat? || x.string? } || prev
     when .brackop? then @val[0] == '(' ? prev : true
     else                prev
     end
--- src/libcv/mt_core/tl_rule/string_rule.cr
+++ src/libcv/mt_core/tl_rule/string_rule.cr
@@ -7,7 +7,7 @@ module CV::TlRule
     key_io = String::Builder.new(root.key)
     val_io = String::Builder.new(root.val)
 
-    while string_component?(node.tag)
+    while string_component?(node)
       key_io << node.key
       val_io << node.val
       break unless node = node.succ?
@@ -21,10 +21,11 @@ module CV::TlRule
     root.fix_succ!(node)
   end
 
-  def string_component?(ptag : PosTag) : Bool
-    case ptag
-    when .pdeci?, .atsgn?, .string? then true
-    else                                 false
+  def string_component?(node : MtNode) : Bool
+    case node.tag
+    when .pdeci?, .atsgn? then node.succ?(&.string?) || false
+    when .string?         then true
+    else                       false
     end
   end
 end
