7284a5fbb37d837213154acd77463786d301c125
Nipinium <nipinium@gmail.com>
2021-10-15 16:41:56 +0700

    convert all adjt rules to nested version

--- src/libcv/mt_core/tl_rule/adjective_rules.cr
+++ src/libcv/mt_core/tl_rule/adjective_rules.cr
@@ -6,59 +6,55 @@ module CV::TlRule
       case succ.tag
       when .adjt?, .amorp?
         node.val = "thật" if node.tag.ahao?
-        node = fold!(node, succ, node.tag)
+        node = fold!(node, succ, PosTag::Adjt)
       when .noun?
-        return node unless node.key.size == 1 # or special case
-        succ = fold_noun!(succ)
-        node.set_prev(succ)
-        return fold!(succ, node, PosTag::Nphrase, dic: 3)
+        break unless node.key.size == 1 && !prev # or special case
+
+        head, tail = swap!(node, succ)
+        return fold!(head, tail, PosTag::Nphrase, dic: 4)
       when .verb?
-        break unless node.key.size == 1
+        break unless node.key.size == 1 && !prev
         succ = fold_verbs!(succ)
 
         if succ.verbs? && node.tag.ahao?
-          node.tag = PosTag::Verb
-          return node.fold!(succ, "thật #{succ.val}")
+          node.val = "thật"
+          return fold!(node, succ, PosTag::Verb, dic: 4)
         end
 
-        node = fold_adj_adv!(node, prev) if prev
-        node.tag = PosTag::Vphrase
-        return node.fold!(succ, "#{succ.val} #{node.val}")
+        head, tail = swap!(node, succ)
+        return fold!(head, tail, PosTag::Vphrase, dic: 4)
       when .ude2?
         break unless succ_2 = succ.succ?
         break unless succ_2.verb? || succ_2.veno?
 
         succ_2 = fold_verbs!(succ_2)
-        node = fold_adj_adv!(node, prev) if prev
+        node = fold_adj_adv!(node, prev)
 
-        node.val = "#{node.val} mà #{succ_2.val}"
-        node.dic = 7
-        node.tag = PosTag::Vphrase
-        return node.fold_many!(succ, succ_2)
-      when .suf_nouns?
-        return fold_suf_noun!(node, succ)
+        succ.val = "mà"
+        return fold!(node, succ_2, PosTag::Vphrase, 4)
       when .uzhi?
-        node = fold_adj_adv!(node, prev) if prev
-        return node = fold_uzhi!(succ, node)
+        node = fold_adj_adv!(node, prev)
+        return fold_uzhi!(succ, node)
+      when .suf_nouns?
+        node = fold_adj_adv!(node, prev)
+        return fold_suf_noun!(node, succ)
       when .suf_verbs?
+        node = fold_adj_adv!(node, prev)
         return fold_suf_verb!(node, succ)
       when .penum?, .concoord?
         break unless (succ_2 = succ.succ?) && can_combine_adjt?(node, succ_2)
         heal_concoord!(succ) if succ.concoord?
-        node = fold!(node, succ, PosTag::Aphrase, 3)
+        node = fold!(node, succ, PosTag::Aphrase, 4)
       when .adv_bu?
         break unless (succ_2 = succ.succ?)
 
         if prev && prev.adv_bu?
-          prev.tag = PosTag::Aphrase
-          prev.dic = 6
-          prev.val = "không #{node.val} không #{node.val}"
-          return prev.fold_many!(node, succ, succ_2)
+          return fold!(prev, succ_2, PosTag::Aphrase, 5)
         elsif succ_2.key == node.key
-          node.tag = PosTag::Aphrase
-          node.dic = 6
-          node.val = "#{node.val} hay không"
-          node.fold_many!(node, succ, succ_2)
+          node = fold_adj_adv!(node, prev)
+          succ.val = "hay"
+          succ_2.val = "không"
+          return fold!(node, succ_2, PosTag::Aphrase, 5)
         end
 
         break
@@ -74,8 +70,13 @@ module CV::TlRule
   end
 
   def fold_modifier!(node : MtNode, succ = node.succ?, nega : MtNode? = nil)
-    # TODO: combine with nouns
-    node.fold_left!(nega)
+    if nega
+      head, tail = swap!(node, nega)
+      node = fold!(head, tail, node.tag, dic: 3)
+    else
+      # TODO: combine with nouns
+      node
+    end
   end
 
   def fold_adj_adv!(node : MtNode, prev = node.prev?)
@@ -84,12 +85,19 @@ module CV::TlRule
     case prev.key
     when "最", "那么", "这么", "非常"
       head, tail = swap!(prev, node)
-      fold!(head, tail, PosTag::Adjt, dic: 2)
+      fold!(head, tail, PosTag::Adjt, dic: 4)
     when "不太"
-      prev.fold!(node, "không #{node.val} lắm")
+      head = MtNode.new("不", "không", PosTag::AdvBu, 1, prev.idx)
+      tail = MtNode.new("太", "lắm", PosTag::Adverb, 1, prev.idx + 1)
+
+      head.fix_prev!(prev.prev?)
+      head.fix_succ!(node)
+      tail.fix_succ!(node.succ?)
+      tail.fix_prev!(node)
+      fold!(head, tail, PosTag::Aphrase, dic: 4)
     else
       prev.val = "rất" if prev.key == "挺"
-      fold!(prev, node, PosTag::Aphrase, dic: 2)
+      fold!(prev, node, PosTag::Aphrase, dic: 4)
     end
   end
 end
