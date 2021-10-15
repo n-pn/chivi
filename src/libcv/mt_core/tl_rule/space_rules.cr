e1f4a0ee0c48191495ccccef3dd31209bad66678
Nipinium <nipinium@gmail.com>
2021-10-15 19:51:18 +0700

    fix space rule

--- src/libcv/mt_core/tl_rule/space_rules.cr
+++ src/libcv/mt_core/tl_rule/space_rules.cr
@@ -1,15 +1,12 @@
 module CV::TlRule
   def fold_noun_space!(node : MtNode, succ = node.succ?) : MtNode
     return node unless succ
-    node.tag = PosTag::Place
 
     case succ.tag
     when .vshang?
       succ.val = "trên"
     when .vxia?
       succ.val = "dưới"
-
-      node.fold!("dưới #{node.val}")
     else
       succ.val = "trước" if succ.key == "之前"
     end
