ffc79f346539d0f985c3988fb6929a72a9350df1
Nipinium <nipinium@gmail.com>
2021-10-17 05:03:43 +0700

    improve meaning of vm_hui

--- src/libcv/mt_core/tl_rule/vmodal_rules.cr
+++ src/libcv/mt_core/tl_rule/vmodal_rules.cr
@@ -14,7 +14,7 @@ module CV::TlRule
   def heal_vm_hui!(node : MtNode, succ = node.succ?, prev = node.prev?) : MtNode
     nega = prev.try(&.adv_bu?)
 
-    if is_learnable_skill?(succ) || prev.try(&.prev?(&.key.== "也"))
+    if is_skill_succ?(succ) || is_skill_prev?(prev.try(&.prev?))
       node.val = "biết"
       prev ? fold!(prev, node, node.tag, dic: 3) : node
     else
@@ -23,14 +23,23 @@ module CV::TlRule
     end
   end
 
-  def is_learnable_skill?(succ : MtNode?) : Bool
-    return false unless succ
+  private def is_skill_succ?(succ : MtNode?) : Bool
+    return true unless succ
     return true if succ.nouns? || succ.exmark? || succ.qsmark?
 
     case succ.key[0]?
     when '打', '说', '做' then true
     else
       {"跳舞", "做饭", "开车", "游泳"}.includes?(succ.key)
+    end
+  end
+
+  private def is_skill_prev?(prev : MtNode?) : Bool
+    return true unless prev
+
+    case prev.key
+    when "都", "也" then true
+    else               false
     end
   end
 
