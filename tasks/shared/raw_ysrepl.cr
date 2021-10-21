2da230537891d74794822a5c9cc3606f7b2fe89d
Nipinium <nipinium@gmail.com>
2021-10-21 10:07:47 +0700

    skip import if content is  请登录查看评论内容

--- tasks/shared/raw_ysrepl.cr
+++ tasks/shared/raw_ysrepl.cr
@@ -36,6 +36,8 @@ class CV::RawYsrepl
   getter created_at : Time
 
   def seed!(bumped : Int64 = Time.utc.to_unix)
+    return if self.ztext == "请登录查看评论内容"
+
     return unless yscrit = Yscrit.find({origin_id: self.yscrit_id})
     ysuser = Ysuser.get!(self.user._id, self.user.name)
 
