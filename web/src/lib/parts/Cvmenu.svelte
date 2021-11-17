356378cbf0e4c2c0d56b916976136a7c90e8b45b
Nipinium <nipinium@gmail.com>
2021-11-17 18:38:46 +0700

    add popup menu when clicking cv node

---
+++ web/src/lib/parts/Cvmenu.svelte
@@ -0,0 +1,102 @@
+<script context="module">
+  import { writable } from 'svelte/store'
+  export const state = writable(0)
+  export const place = writable([100, 100])
+  export const input = writable([''])
+
+  import { activate as lookup_activate } from '$parts/Lookup.svelte'
+  import { activate as upsert_activate } from '$parts/Upsert.svelte'
+
+  import { state as tlspec_state } from '$parts/Tlspec.svelte'
+</script>
+
+<script>
+  import SIcon from '$atoms/SIcon.svelte'
+
+  $: [top, left] = $place
+</script>
+
+<button data-kbd="x" on:click={() => upsert_activate($input, 0)}>X</button>
+<button data-kbd="c" on:click={() => upsert_activate($input, 1)}>C</button>
+<button data-kbd="enter" on:click={() => upsert_activate($input, 0)}>E</button>
+
+<cvmenu-wrap class:active={$state > 0} style="--top: {top}px; --left: {left}px">
+  <cvmenu-item
+    data-tip="Từ điển"
+    tip-loc="bottom"
+    on:click={() => upsert_activate($input, 0)}>
+    <SIcon name="pencil" />
+  </cvmenu-item>
+
+  <cvmenu-item
+    data-tip="Tra từ"
+    tip-loc="bottom"
+    on:click={() => lookup_activate($input)}>
+    <SIcon name="search" />
+  </cvmenu-item>
+
+  <cvmenu-item
+    data-tip="Báo lỗi"
+    tip-loc="bottom"
+    on:click={() => ($tlspec_state = 1)}>
+    <SIcon name="flag" />
+  </cvmenu-item>
+</cvmenu-wrap>
+
+<style lang="scss">
+  $size: 2rem;
+
+  cvmenu-wrap {
+    height: $size;
+    z-index: 9;
+    display: flex;
+    position: fixed;
+    height: $size;
+    width: 6rem;
+    padding: 0 0.25rem;
+
+    top: var(--top, 20vw);
+    left: var(--left, 20vw);
+    @include fgcolor(white);
+    @include bgcolor(black);
+    @include bdradi();
+    @include shadow();
+
+    visibility: hidden;
+
+    &.active {
+      visibility: visible;
+    }
+
+    &:before {
+      display: block;
+      position: absolute;
+      content: ' ';
+      bottom: 100%;
+      left: 50%;
+      margin-left: -0.375rem;
+
+      border: 0.375rem solid transparent;
+      border-bottom-color: color(black);
+    }
+  }
+
+  cvmenu-item {
+    width: $size;
+    height: 100%;
+    @include flex-ca;
+
+    &:hover {
+      cursor: pointer;
+      @include fgcolor(primary, 5);
+    }
+
+    // &:first-child {
+    //   @include bdradi($loc: left);
+    // }
+
+    // &:last-child {
+    //   @include bdradi($loc: right);
+    // }
+  }
+</style>
--- web/src/lib/sects/Cvdata.svelte
+++ web/src/lib/sects/Cvdata.svelte
@@ -1,26 +1,24 @@
 <script context="module">
-  import Lookup, {
-    activate as lookup_activate,
-    enabled as lookup_enabled,
-  } from '$parts/Lookup.svelte'
-
-  import Upsert, {
-    state as upsert_state,
-    activate as upsert_activate,
-  } from '$parts/Upsert.svelte'
 </script>
 
 <script>
   import { onMount } from 'svelte'
+  import { browser } from '$app/env'
   import { page } from '$app/stores'
-  import { session } from '$app/stores'
 
   import { ftsize } from '$lib/stores'
   import { MtData } from '$lib/mt_data'
   import read_selection from '$utils/read_selection'
 
-  import SIcon from '$atoms/SIcon.svelte'
+  import Cvmenu, {
+    state as cvmenu_state,
+    place as cvmenu_place,
+    input,
+  } from '$parts/Cvmenu.svelte'
+
   import Tlspec, { state as tlspec_state } from '$parts/Tlspec.svelte'
+  import Lookup, { enabled as lookup_enabled } from '$parts/Lookup.svelte'
+  import Upsert, { state as upsert_state } from '$parts/Upsert.svelte'
 
   export let cvdata = ''
   export let zhtext = []
@@ -39,12 +37,11 @@
   let focus_line = 0
   let focus_word = null
 
-  let selected = []
   onMount(() => {
     const action = document.addEventListener('selectionchange', () => {
       if (hover_line < 0) return
       const [lower, upper] = read_selection()
-      if (upper > 0) selected = [zhtext[hover_line], lower, upper]
+      if (upper > 0) $input = [zhtext[hover_line], lower, upper]
     })
 
     return () => document.removeEventListener('selectionchange', action)
@@ -52,20 +49,28 @@
 
   function handle_click({ target }, index) {
     if (focus_line != index) focus_line = index
-    if (target.nodeName != 'C-V') return
+    if (target.nodeName != 'C-V') {
+      $cvmenu_state = 0
+      return
+    } else {
+      const rects = target.getClientRects()
+      const rect = rects[rects.length - 1]
+
+      let left = Math.floor((rect.left + rect.right) / 2) - 48
+      if (left < 0) left = 0
+      else if (left + 96 > window.innerWidth) left = window.innerWidth - 96
+
+      $cvmenu_place = [rect.bottom + 6, left]
+      $cvmenu_state = 1
+    }
 
     const lower = +target.dataset.i
     const upper = lower + +target.dataset.l
-    selected = [zhtext[index], lower, upper]
+    $input = [zhtext[index], lower, upper]
 
-    if (target === focus_word) {
-      upsert_activate(selected, 0)
-    } else {
-      if (focus_word) focus_word.classList.remove('_focus')
-      focus_word = target
-      focus_word.classList.add('_focus')
-      lookup_activate(selected)
-    }
+    if (focus_word) focus_word.classList.remove('_focus')
+    focus_word = target
+    focus_word.classList.add('_focus')
   }
 
   function render_line(idx = 0, hover = -1, focus = -1, debug = false) {
@@ -79,10 +84,6 @@
 <div hidden>
   <button data-kbd="g" on:click={() => (debug = !debug)}>G</button>
   <button data-kbd="r" on:click={on_change}>R</button>
-  <button data-kbd="x" on:click={() => upsert_activate(selected, 0)}>X</button>
-  <button data-kbd="c" on:click={() => upsert_activate(selected, 1)}>C</button>
-  <button data-kbd="enter" on:click={() => upsert_activate(selected, 0)}
-    >E</button>
 </div>
 
 <article class="cvdata _{$ftsize}" class:debug>
@@ -95,33 +96,26 @@
       on:click={(e) => handle_click(e, index)}
       on:mouseenter={() => (hover_line = index)}>
       {@html render_line(index, hover_line, focus_line, debug)}
-
-      <!-- {#if $session.privi >= 0}
-        <button
-          class="report-line"
-          data-tip="Báo lỗi dịch thuật"
-          on:click={() => ($tlspec_state = 1)}>
-          <SIcon name="flag" />
-        </button>
-      {/if} -->
     </div>
   {/each}
 </article>
 
-{#if $lookup_enabled}
+{#if browser}
+  {#if $upsert_state}
+    <Upsert {dname} {d_dub} {on_change} />
+  {/if}
+
   <Lookup {dname} />
-{/if}
 
-{#if $upsert_state}
-  <Upsert {dname} {d_dub} {on_change} />
-{/if}
+  {#if $tlspec_state}
+    <Tlspec
+      {dname}
+      {d_dub}
+      ztext={zhtext[hover_line]}
+      slink="{$page.path}#L{hover_line}" />
+  {/if}
 
-{#if $tlspec_state}
-  <Tlspec
-    {dname}
-    {d_dub}
-    ztext={zhtext[hover_line]}
-    slink="{$page.path}#L{hover_line}" />
+  <Cvmenu />
 {/if}
 
 <style lang="scss">
