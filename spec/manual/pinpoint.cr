42ac7bb9c141b5139c3dee31d64b5da7c1423775
Nipinium <nipinium@gmail.com>
2021-12-03 21:51:33 +0700

    only group noun with adjt if nesseary

--- spec/manual/pinpoint.cr
+++ spec/manual/pinpoint.cr
@@ -2,7 +2,7 @@ require "../../src/cvmtl/mt_core"
 
 GENERIC = CV::MtCore.generic_mtl("7zx48vye")
 
-inp = "大的那个是男款"
+inp = "这两件衣服一大一小，大的那个是男款，女款则是很小的童装，毫无疑问，这正是一周前罗亚亲自去索罗菲亚商会订下的衣服。"
 res = GENERIC.cv_plain(inp)
 
 puts inp, res.inspect, res
