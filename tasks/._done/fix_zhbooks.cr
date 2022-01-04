e74693d3f367faf118b65c340db3e1b2e0a74fd6
Nipinium <nipinium@gmail.com>
2022-01-04 13:43:01 +0700

    mark task as done

---
+++ tasks/._done/fix_zhbooks.cr
@@ -0,0 +1,21 @@
+require "./shared/bootstrap"
+
+# CV::Nvinfo.query.each_with_cursor(20) do |nvinfo|
+#   zhbooks = nvinfo.zhbooks.to_a
+#   next if zhbooks.empty?
+
+#   zhbooks.each do |zhbook|
+#     zhbook.nvinfo = nvinfo
+#     zhbook.zseed = CV::NvSeed.map_id(zhbook.sname)
+#     zhbook.fix_id!
+#     zhbook.save!
+#   end
+
+#   nvinfo.update({zseed_ids: zhbooks.map(&.zseed).sort})
+# end
+
+CV::Ubmemo.query.each_with_cursor(20) do |ubmemo|
+  ubmemo.lr_sname = "chivi"
+  ubmemo.lr_zseed = CV::NvSeed.map_id(ubmemo.lr_sname)
+  ubmemo.save!
+end
