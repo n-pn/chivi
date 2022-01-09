ab77b845fc2a1ebd506ccbe9393a96ea08c3b03f
Nipinium <nipinium@gmail.com>
2022-01-10 01:43:30 +0700

    support mapping fullwidth numbers

--- spec/manual/pinpoint.cr
+++ spec/manual/pinpoint.cr
@@ -2,7 +2,7 @@ require "../../src/cvmtl/mt_core"
 
 GENERIC = CV::MtCore.generic_mtl("82qjzt29")
 
-inp = "第二集 行湿走肉（TheFuckingDead）S1E2"
+inp = "３０００多人感染"
 res = GENERIC.cv_title_full(inp)
 
 {res.inspect, inp, res}.each do |text|
--- src/cvmtl/vp_dict/pos_tag/numbers.cr
+++ src/cvmtl/vp_dict/pos_tag/numbers.cr
@@ -18,7 +18,7 @@ struct CV::PosTag
   Nqverb = new(Tag::Nqverb, NQ_POS)
   Nqiffy = new(Tag::Nqiffy, NQ_POS) # unknown nquants
 
-  NUMLAT_RE = /^\d+$/
+  NUMLAT_RE = /^[\d０-９]+$/
   NUMHAN_RE = /^[零〇一二两三四五六七八九十百千万亿兆]+$/
 
   def self.map_numbers(key : ::String) : self
