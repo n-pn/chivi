7e40c01f485458fd5cefab4a96661153ed5f2da6
Nipinium <nipinium@gmail.com>
2022-01-15 22:07:55 +0700

    add routes

--- web/src/routes/[...slug].svelte
+++ web/src/routes/[...slug].svelte
@@ -1,6 +1,8 @@
 <script context="module">
   export async function load({ params: { slug } }) {
-    if (slug.startsWith('guide/')) {
+    if (slug.startsWith('cvmtl')) {
+      return { status: 301, redirect: slug.replace('cvmtl', '/qtran') }
+    } else if (slug.startsWith('guide')) {
       return { status: 301, redirect: slug.replace('guide', '/notes') }
     } else {
       return { status: 404, error: `${slug} not found!` }
