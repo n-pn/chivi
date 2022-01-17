b9355825869c364115788070d66bc08dd39887ab
Nipinium <nipinium@gmail.com>
2022-01-18 05:39:27 +0700

    tweak

--- spec/manual/debug_engine.cr
+++ spec/manual/debug_engine.cr
@@ -2,7 +2,7 @@ require "../../src/cvmtl/mt_core"
 
 GENERIC = CV::MtCore.generic_mtl("combine")
 
-inp = "忍不住仔细打量起那张扭曲的脸庞"
+inp = "有些控制不住"
 res = GENERIC.cv_plain(inp)
 
 {res.inspect, inp, res}.each do |text|
