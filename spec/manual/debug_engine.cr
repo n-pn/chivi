80e89326a02c829299d911d0b9cf2a0f910f3f74
Nipinium <nipinium@gmail.com>
2022-01-19 05:59:19 +0700

    add 又 as concoord

--- spec/manual/debug_engine.cr
+++ spec/manual/debug_engine.cr
@@ -2,7 +2,7 @@ require "../../src/cvmtl/mt_core"
 
 GENERIC = CV::MtCore.generic_mtl("m1596jjw")
 
-inp = "那张既帅气又正义的小白脸"
+inp = "瞪大，"
 res = GENERIC.cv_plain(inp)
 
 {res.inspect, inp, res}.each do |text|
