ea446171b7f23d8be9dc35da8753ec57fa8b3e03
Nipinium <nipinium@gmail.com>
2022-01-18 16:24:24 +0700

    fold noun + noun if before is preposes

--- spec/manual/debug_engine.cr
+++ spec/manual/debug_engine.cr
@@ -1,8 +1,8 @@
 require "../../src/cvmtl/mt_core"
 
-GENERIC = CV::MtCore.generic_mtl("fnjqty5r")
+GENERIC = CV::MtCore.generic_mtl("m1596jjw")
 
-inp = "小白狗飞快地跑出去"
+inp = "往苏荆脸上"
 res = GENERIC.cv_plain(inp)
 
 {res.inspect, inp, res}.each do |text|
