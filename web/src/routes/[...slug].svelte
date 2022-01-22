7a07c340571bd387bd0be535f52e40f749471def
Nipinium <nipinium@gmail.com>
2022-01-23 00:06:36 +0700

    fix inifinity redirect

--- web/src/routes/[...slug].svelte
+++ web/src/routes/[...slug].svelte
@@ -4,7 +4,7 @@
       return { status: 301, redirect: slug.replace('@', '/books/@') }
     } else if (slug.startsWith('cvmtl')) {
       return { status: 301, redirect: slug.replace('cvmtl', '/qtran') }
-    } else if (slug.startsWith('guide')) {
+    } else if (slug.startsWith('notes')) {
       return { status: 301, redirect: slug.replace('notes', '/guide') }
     } else {
       return { status: 404, error: `${slug} not found!` }
