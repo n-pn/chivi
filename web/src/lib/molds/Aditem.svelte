baf8351dccd4655d68f14cbbbee754c7c1a602cb
Nipinium <nipinium@gmail.com>
2022-02-04 21:16:12 +0700

    improve ads placement

--- web/src/lib/molds/Aditem.svelte
+++ web/src/lib/molds/Aditem.svelte
@@ -3,18 +3,20 @@
 </script>
 
 <section>
-  {@html `<ins class="adsbygoogle" style="display:block; text-align:center;"
+  <ins
+    class="adsbygoogle"
+    style="display:block; text-align:center;"
     data-ad-client="ca-pub-5468438393284967"
-    data-ad-slot="${type == 'banner' ? '2323076740' : '1006213923'}"
-    data-ad-format="${type == 'banner' ? 'auto' : 'fluid'}"
-    data-ad-layout="${type == 'banner' ? '' : 'in-article'}"
-    data-full-width-responsive="true" />`}
+    data-ad-slot={type == 'banner' ? '2323076740' : '1006213923'}
+    data-ad-format={type == 'banner' ? 'auto' : 'fluid'}
+    data-ad-layout={type == 'banner' ? '' : 'in-article'}
+    data-full-width-responsive="true" />
   <div>
     <p>Bạn đang bật adblock, hoặc adsense vì lý do nào đó không chạy :(</p>
     <p>Quảng cáo là yếu tố thiết yếu để trang tiếp tục tồn tại.</p>
     <p>
-      Xin hãy tắt adblock, hoặc nâng cấp quyền hạn lên **2** để không phải xem
-      quảng cáo.
+      Xin hãy tắt adblock, hoặc nâng cấp quyền hạn lên <strong>2</strong> để không
+      phải xem quảng cáo.
     </p>
   </div>
 
@@ -28,18 +30,22 @@
     margin: 1rem 0;
   }
 
-  :global(.adsbygoogle:empty + div) {
+  ins:empty + div {
     padding: 0.75rem;
-    line-height: 1.25rem;
-    text-transform: uppercase;
-    font-size: 500;
+    text-align: center;
 
     @include bdradi();
     @include fgcolor(neutral, 7);
     @include bgcolor(harmful, 1);
   }
 
-  p + p {
-    margin-top: 0.75rem;
+  ins:not(:empty) + div {
+    display: none;
+  }
+
+  p {
+    font-size: 500;
+    margin: 0;
+    @include ftsize(lg);
   }
 </style>
