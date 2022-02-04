e6537ae455973d16d2ed398b6f9f43f071703fcd
Nipinium <nipinium@gmail.com>
2022-02-04 19:06:42 +0700

    make aditem content inside html directive

--- web/src/lib/molds/Aditem.svelte
+++ web/src/lib/molds/Aditem.svelte
@@ -3,26 +3,17 @@
 </script>
 
 <section>
-  <ins
-    class="adsbygoogle"
-    style="display:block; text-align:center;"
+  {@html `<ins class="adsbygoogle" style="display:block; text-align:center;"
     data-ad-client="ca-pub-5468438393284967"
-    data-ad-slot={type == 'banner' ? '2323076740' : '1006213923'}
-    data-ad-format={type == 'banner' ? 'auto' : 'fluid'}
-    data-ad-layout={type == 'banner' ? '' : 'in-article'}
-    data-full-width-responsive="true" />
-  <script>
-    try {
-      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
-    } catch (aderr) {
-      console.log({ aderr })
-    }
-  </script>
+    data-ad-slot=${type == "banner" ? "2323076740" : "1006213923"}
+    data-ad-format=${type == "banner" ? 'auto' : "fluid"}
+    data-ad-layout=${type == "banner" ? '' : "in-article"}
+    data-full-width-responsive="true" />`}
+  {@html "<script>;(window.adsbygoogle = window.adsbygoogle || []).push({})</script>"}
 </section>
 
 <style lang="scss">
-  ins {
-    max-width: 100%;
+  section {
     margin: 1rem 0;
   }
 </style>
