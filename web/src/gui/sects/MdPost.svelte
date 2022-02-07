d122df4fa6ac123d823d90ac0ea5d109ef471afd
Nipinium <nipinium@gmail.com>
2022-02-07 23:56:26 +0700

    remove deprecated Vessel component

--- web/src/gui/sects/MdPost.svelte
+++ web/src/gui/sects/MdPost.svelte
@@ -1,15 +1,12 @@
 <script>
   export let title
-  import Vessel from './Vessel.svelte'
 </script>
 
 <svelte:head>
   <title>{title} - Chivi</title>
 </svelte:head>
 
-<Vessel>
-  <article class="md-post">
-    <h1>{title}</h1>
-    <slot />
-  </article>
-</Vessel>
+<article class="md-post">
+  <h1>{title}</h1>
+  <slot />
+</article>
