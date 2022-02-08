f6da547b488e39bbcd06aafe9b2a8b30211a73b8
Nipinium <nipinium@gmail.com>
2022-02-09 04:52:39 +0700

    fix nquant as verb complement

--- src/cvmtl/mt_core/tl_rule/verb_rules/fold_verb_nquant.cr
+++ src/cvmtl/mt_core/tl_rule/verb_rules/fold_verb_nquant.cr
@@ -1,24 +1,28 @@
 module CV::TlRule
-  def fold_verb_nquant!(verb : MtNode, node : MtNode, has_ule = false)
-    node = fuse_number!(node) if node.numbers?
-    return verb unless node.nquants?
+  def fold_verb_nquant!(verb : MtNode, tail : MtNode, has_ule = false)
+    tail = fuse_number!(tail) if tail.numbers?
+    return verb unless tail.nquants?
 
-    if node.nqiffy? || node.nqnoun?
-      return verb unless !has_ule || is_temp_nqverb?(node)
+    puts [verb, tail]
+
+    if tail.nqiffy? || tail.nqnoun?
+      return verb if has_ule || !is_temp_nqverb?(tail)
     end
 
-    return fold!(verb, node, verb.tag, dic: 6)
+    return fold!(verb, tail, verb.tag, dic: 6)
   end
 
-  def is_temp_nqverb?(node : MtNode)
-    case node.key[-1]?
-    when '把'
-      return false unless node.succ?(&.noun?)
-      node.val = node.val.sub("bả", "phát")
-    when '脚', '眼', '圈', '次' # , '口'
-      true
-    else
-      false
+  def is_temp_nqverb?(tail : MtNode)
+    tail.each do |x|
+      case x.key[-1]?
+      when '把'
+        return false if tail.succ?(&.noun?)
+        x.val = x.val.sub(/bả|chiếc/, "phát")
+      when '脚', '眼', '圈', '次' # , '口'
+        return true
+      end
     end
+
+    false
   end
 end
