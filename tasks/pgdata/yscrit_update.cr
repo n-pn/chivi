8b9a4ff59a6a3463f7a6731e86e9cb23296b1404
Nipinium <nipinium@gmail.com>
2022-02-09 16:36:43 +0700

    trace errors

--- tasks/pgdata/yscrit_update.cr
+++ tasks/pgdata/yscrit_update.cr
@@ -1,6 +1,22 @@
 require "../shared/seed_util"
 
+DEBUG = ARGV.includes?("debug")
+
 CV::Yscrit.query.order_by(id: :asc).with_nvinfo.each_with_cursor(10) do |yscrit|
+  if DEBUG
+    File.open("tmp/yscrit-err.log", "w") do |io|
+      io.puts yscrit.nvinfo.bhash
+      yscrit.zhtext.join(io, '\n')
+    end
+  end
+
   vhtml = CV::SeedUtil.cv_ztext(yscrit.zhtext, yscrit.nvinfo.bhash)
   yscrit.update!({vhtml: vhtml})
+rescue err
+  File.open("tmp/yscrit-#{yscrit.id}.log", "w") do |io|
+    io.puts yscrit.nvinfo.bhash
+    yscrit.zhtext.join(io, '\n')
+    io.puts "----"
+    err.inspect_with_backtrace(io)
+  end
 end
