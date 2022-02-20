01953cccdadd41a68ea3c44af89c4d2c264ef4c4
Nipinium <nipinium@gmail.com>
2022-02-21 05:45:19 +0700

    fix user backup script

--- tasks/maintain/user_backup.cr
+++ tasks/maintain/user_backup.cr
@@ -3,53 +3,23 @@ require "./_shared"
 module CV::UserBackup
   extend self
 
-  def user_file(uname : String)
-    File.join(CVUSER_DIR, uname.downcase + ".tsv")
-  end
-
-  FIELDS = %w(id uname email cpass karma privi wtheme)
-  STAMPS = %w(created_at updated_at)
-
   def save_cvuser(user : Cvuser)
-    store = Tabkv.new(user_file(user.uname))
+    user_file = "#{CVUSER_DIR}/#{user.id}.json"
+    memo_file = "#{CVUSER_DIR}/memos/#{user.id}.txt"
 
-    {% for field in FIELDS %}
-      store.set!({{field}}, user.{{field.id}})
-    {% end %}
+    File.write(user_file, user.to_pretty_json)
 
-    store.set!("privi_until", user.privi_until.try(&.to_unix))
-
-    {% for field in STAMPS %}
-      store.set!({{field}}, user.{{field.id}}.to_unix)
-    {% end %}
-
-    store.save!
-  end
-
-  def save_ubmemo(user : Cvuser)
-    status = Tabkv.new(user_file("memos/" + user.uname + "-status"))
-    access = Tabkv.new(user_file("memos/" + user.uname + "-access"))
-
-    Ubmemo.query.where({cvuser_id: user.id}).with_nvinfo.each do |entry|
-      value_1 = [entry.status_s, entry.locked, entry.atime, entry.created_at.to_unix, entry.updated_at.to_unix]
-      status.set!(entry.nvinfo.bhash, value_1.map(&.to_s))
-
-      value_2 = [entry.lr_sname, entry.lr_chidx, entry.lr_cpart, entry.utime, entry.lc_title, entry.lc_uslug]
-      access.set!(entry.nvinfo.bhash, value_2.map(&.to_s))
+    File.open(memo_file, "w") do |io|
+      Ubmemo.query.where({cvuser_id: user.id}).with_nvinfo.each do |entry|
+        io << entry.nvinfo.bhash << '\t' << entry.to_json << '\n'
+      end
     end
-
-    status.save!
-    access.save!
   end
 
   def run!(fresh = false)
     FileUtils.rm_rf(CVUSER_DIR) if fresh
     FileUtils.mkdir_p(CVUSER_DIR + "/memos")
-
-    Cvuser.query.order_by(id: :asc).each do |user|
-      save_cvuser(user)
-      save_ubmemo(user)
-    end
+    Cvuser.query.order_by(id: :asc).each { |user| save_cvuser(user) }
   end
 end
 
