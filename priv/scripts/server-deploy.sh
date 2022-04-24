204893e84f80f7b5d156265b6934ab5ee98eb183
Nipinium <nipinium@gmail.com>
2022-04-25 06:18:56 +0700

    update script

--- priv/scripts/server-deploy.sh
+++ priv/scripts/server-deploy.sh
@@ -4,7 +4,7 @@ if [[ $1 == "all" || $1 == "chivi-srv" ]]
 then
   echo push server!
 
-  shards build --release chivi && rsync -aiz --no-p bin/chivi $SSH/bin
+  shards build -s --release chivi && rsync -aiz --no-p bin/chivi $SSH/bin
   ssh nipin@ssh.chivi.app "sudo service chivi-srv restart"
 fi
 
@@ -21,7 +21,7 @@ if [[ $1 == "all" || $1 == "ysbook" ]]
 then
   echo push ysbook seed!
 
-  shards build --release ysbook_seed
+  shards build -s --release ysbook_seed
   rsync -aiz --no-p bin/ysbook_seed $SSH/bin
   rsync -azi --no-p "var/ysinfos/ysbooks" "$SSH/var/ysinfos"
 fi
@@ -30,7 +30,7 @@ if [[ $1 == "all" || $1 == "ysrepl" ]]
 then
   echo push ysrepl seed!
 
-  crystal build --release tasks/pgdata/ysrepl_seed.cr -o bin/ysrepl_seed
+  crystal build -s --release tasks/pgdata/ysrepl_seed.cr -o bin/ysrepl_seed
 
   rsync -aiz --no-p bin/ysrepl_seed $SSH/bin
   rsync -azi --no-p "var/ysinfos/ysrepls" "$SSH/var/ysinfo"
@@ -40,7 +40,7 @@ if [[ $1 == "all" || $1 == "zhinfo" ]]
 then
   echo push zhinfo seed!
 
-  shards build --release zhinfo_seed
+  shards build -s --release zhinfo_seed
   rsync -aiz --no-p bin/zhinfo_seed $SSH/bin
   rsync -azi --no-p "var/zhinfos" "$SSH/var"
 fi
@@ -48,13 +48,13 @@ fi
 if [[ $1 == "all" || $1 == "fixes" ]]
 then
   echo push fixes binaries!
-  shards build --release fix_genres && rsync -aiz --no-p bin/fix_genres $SSH/bin
-  shards build --release fix_intros && rsync -aiz --no-p bin/fix_intros $SSH/bin
+  shards build -s --release fix_genres && rsync -aiz --no-p bin/fix_genres $SSH/bin
+  shards build -s --release fix_intros && rsync -aiz --no-p bin/fix_intros $SSH/bin
 fi
 
 if [[ $1 == "all" || $1 == "binary" ]]
 then
   echo push binaries!
-  shards build --release ysbook_seed && rsync -aiz --no-p bin/ysbook_seed $SSH/bin
-  shards build --release zhinfo_seed && rsync -aiz --no-p bin/zhinfo_seed $SSH/bin
+  shards build -s --release ysbook_seed && rsync -aiz --no-p bin/ysbook_seed $SSH/bin
+  shards build -s --release zhinfo_seed && rsync -aiz --no-p bin/zhinfo_seed $SSH/bin
 fi
