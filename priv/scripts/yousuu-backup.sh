b2bb0d32a03853ad82ff5bbe870c5aaea8148790
Nipinium <nipinium@gmail.com>
2022-04-30 04:44:36 +0700

    update backup script

--- priv/scripts/yousuu-backup.sh
+++ priv/scripts/yousuu-backup.sh
@@ -4,14 +4,15 @@ DIR=_db/yousuu
 SSH=nipin@ssh.chivi.app:srv/chivi
 
 rsync -azui --no-p "$SSH/$DIR/infos" "$DIR"
-rsync -azui --no-p "$SSH/$DIR/crits" "$DIR"
-rsync -azui --no-p "$SSH/$DIR/repls" "$DIR"
+rsync -azui --no-p "$SSH/$DIR/users" "$DIR"
 
 rsync -azui --no-p "$SSH/$DIR/lists" "$DIR"
-rsync -azui --no-p "$SSH/$DIR/list-infos" "$DIR"
+rsync -azui --no-p "$SSH/$DIR/lists-by-page" "$DIR"
 rsync -azui --no-p "$SSH/$DIR/lists-by-book" "$DIR"
+rsync -azui --no-p "$SSH/$DIR/lists-by-user" "$DIR"
 
-rsync -azui --no-p "$SSH/$DIR/list-books" "$DIR"
-rsync -azui --no-p "$SSH/$DIR/list-books-by-score" "$DIR"
+rsync -azui --no-p "$SSH/$DIR/crits" "$DIR"
+rsync -azui --no-p "$SSH/$DIR/crits-by-list" "$DIR"
+rsync -azui --no-p "$SSH/$DIR/crits-by-user" "$DIR"
 
-# rsync -azui --no-p "$SSH/var/ysinfos" "var"
+rsync -azui --no-p "$SSH/$DIR/repls" "$DIR"
