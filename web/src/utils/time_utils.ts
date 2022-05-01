f280f38d258430fe72c48b2faec60396eb3a10c2
Nipinium <nipinium@gmail.com>
2022-05-01 22:40:28 +0700

    fix rel_time

--- web/src/utils/time_utils.ts
+++ web/src/utils/time_utils.ts
@@ -20,7 +20,7 @@ export function rel_time_diff(diff: number, suffix = ' trước') {
 export function get_dmy(date: Date) {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
-  const day = date.getDate() + 1
+  const day = date.getDate()
   return `${pad_zero(day)}/${pad_zero(month)}/${year}`
 }
 
