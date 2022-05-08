d9b66aab98c372a688604841496b35f241f20c9c
Nipinium <nipinium@gmail.com>
2022-05-08 07:56:18 +0700

    move engine test to spec folder, and khi-luc-thoi-gian.tsv spec

--- spec/engine/cvmtl_spec.cr
+++ spec/engine/cvmtl_spec.cr
@@ -6,11 +6,11 @@ def convert(input : String)
   MTL.cv_plain(input, cap_first: false).to_s
 end
 
-files = Dir.glob("var/tlspecs/*.tsv")
+files = Dir.glob("spec/engine/cases/*.tsv")
 
 files.each do |file|
   suite_name = File.basename(file, ".tsv")
-  next if suite_name.starts_with?("_")
+  next if suite_name.starts_with?("#")
 
   describe suite_name do
     lines = File.read_lines(file).reject(&.empty?)
