fd5cc731f6b778eb931a369ee7821b36d301ded2
Nipinium <nipinium@gmail.com>
2022-05-12 15:10:35 +0700

    upload base_backup to aws

--- tasks/backup/aws-s3.sh
+++ tasks/backup/aws-s3.sh
@@ -1,5 +1,7 @@
-aws s3 sync var/weblogs s3://chivi-bak/weblogs
-aws s3 sync var/vpdicts/v1 s3://chivi-bak/vpdicts
-aws s3 sync var/tlspecs/users s3://chivi-bak/tlspecs
+aws s3 sync --delete var/pg_data/weblogs s3://chivi-bak/weblogs
+aws s3 sync --delete var/pg_data/cvusers s3://chivi-bak/cvusers
+aws s3 sync --delete var/vpdicts/v1 s3://chivi-bak/vpdicts
+aws s3 sync --delete var/tlspecs/users s3://chivi-bak/tlspecs
 
-aws s3 sync ~/var/wal_log s3://chivi-bak/wal_log
+aws s3 sync --delete ~/var/wal_log s3://chivi-bak/wal_log
+aws s3 sync --delete ~/var/pg_data s3://chivi-bak/pg_data
