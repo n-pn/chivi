bf0fec63e85138379c95a55c82564a620aadbf8f
Nipinium <nipinium@gmail.com>
2022-06-13 08:41:16 +0700

    wording

--- src/webcv/mailers/vcoin_mailer.cr
+++ src/webcv/mailers/vcoin_mailer.cr
@@ -10,7 +10,7 @@ class CV::UvcoinMailer < CV::BaseMailer
       <p>Thông báo từ Chivi</p>
       <p>Bạn nhận được: <strong>#{uvcoin.amount}</strong> vcoin từ #{uvcoin.sender.uname}.</p>
       <p>Chú thích người tặng: #{uvcoin.reason}</p>
-      <p>Bạn có thể dùng vcoin để nâng cấp quyền hạn, để tăng trải nghiệm dùng Chivi.</p>
+      <p>Bạn có thể dùng vcoin để nâng cấp quyền hạn, tăng trải nghiệm khi sử dụng Chivi.</p>
     HTML
   end
 end
