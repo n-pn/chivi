8eb459446267205a4b340655bc74db43d9ea4a46
Nipinium <nipinium@gmail.com>
2022-06-27 10:05:40 +0700

    marrk bxwxorg as dead remote

--- src/appcv/shared/sname_map.cr
+++ src/appcv/shared/sname_map.cr
@@ -38,14 +38,15 @@ module CV::SnameMap
 
   def map_type(sname : String)
     case sname
-    when "=base", "=user"
+    when "=base", "=user", "users"
       0 # act as mirror
     when "zxcs_me", .starts_with?('@')
       1 # manual update
-    when "jx_la", "zhwenpg", "shubaow", "sdyfcm",
-         "duokan8", "biquyue", "xswang", "biqugee"
+    when "jx_la", "zhwenpg", "shubaow", "sdyfcm", "biqugee",
+         "bxwxorg"
       2 # dead remote
-    when "paoshu8", "hetushu", "5200", "biqu5200", "ptwxz"
+    when "paoshu8", "hetushu", "5200", "biqu5200", "ptwxz",
+         "duokan8"
       3  # slow but still alive
     else # "bxwxorg", "69shu", "xbiquge", "rengshu", "biqugee"
       4  # fast remote
