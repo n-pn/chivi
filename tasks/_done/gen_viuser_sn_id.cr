9e57dc06268bf6cefea9ea5927b8813e37fbb50c
Nipinium <nipinium@gmail.com>
2022-09-29 15:52:23 +0700

    update gen_sn_id

--- tasks/_done/gen_viuser_sn_id.cr
+++ tasks/_done/gen_viuser_sn_id.cr
@@ -8,3 +8,15 @@ users.each do |user|
   sn_id = user.id * 2 + 20
   CV::ChSeed.add_user(sname, sn_id)
 end
+
+users_2 = Dir.children("var/chtexts").select(&.starts_with?('@'))
+users_2.each do |uname|
+  next if CV::ChSeed.has_sname?(uname)
+
+  if user = CV::Viuser.find({uname: uname[1..]})
+    sn_id = user.id * 2 + 20
+    CV::ChSeed.add_user(uname, sn_id)
+  else
+    puts "user #{uname} does not exists!"
+  end
+end
