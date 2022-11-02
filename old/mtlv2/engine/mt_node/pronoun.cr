0c20b38c781902ba98154fa6e36a8e96cde24f50
Nipinium <nipinium@gmail.com>
2022-11-02 22:05:15 +0700

    wip: delete dead codes

--- old/mtlv2/engine/mt_node/pronoun.cr
+++ old/mtlv2/engine/mt_node/pronoun.cr
@@ -1,70 +1,31 @@
-require "./_generic"
-
-module MT::MTL
-  # 代词 - pronoun - đại từ chưa phân loại
-  class PronounWord < BaseWord
-    def noun_prefix?
-      true
-    end
-  end
-
-  # 人称代词 - personal pronoun - đại từ nhân xưng
-  class PersproWord < PronounWord
-  end
-
-  # 自己
-  class ProZiji < PersproWord
-  end
-
-  # 指示代词 - demonstrative pronoun - đại từ chỉ thị
-  class DemsproWord < PronounWord
-    def noun_prefix?
-      true
-    end
-  end
-
-  class ProZhe < DemsproWord
-    def heal!(succ = @succ) : Nil
-      case succ
-      when Nil, Prepos
-        @val = "cái này"
-      when Verbal
-        @val = "đây"
-      when Comma
-        if (zhe2 = succ.succ?) && zhe2.is_a?(ProZhe)
-          zhe2.heal!
-          @val = zhe2.val
-        end
-      else
-        @val = "giờ" if @prev.is_a?(Nominal)
+class ProZhe
+  def heal!(succ = @succ) : Nil
+    case succ
+    when Nil, Prepos
+      @val = "cái này"
+    when Verbal
+      @val = "đây"
+    when Comma
+      if (zhe2 = succ.succ?) && zhe2.is_a?(ProZhe)
+        zhe2.heal!
+        @val = zhe2.val
       end
-    end
-
-    def noun_prefix?
-      false
+    else
+      @val = "giờ" if @prev.is_a?(Nominal)
     end
   end
 
-  class ProNa1 < DemsproWord
-    def heal!(succ = @succ) : Nil
-      @val = "vậy" unless succ.is_a?(Verbal)
-    end
+  def noun_prefix?
+    false
+  end
+end
 
-    def noun_prefix?
-      false
-    end
+class ProNa1
+  def heal!(succ = @succ) : Nil
+    @val = "vậy" unless succ.is_a?(Verbal)
   end
 
-  class ProJi3 < DemsproWord
-  end
-
-  # 疑问代词 - interrogative pronoun - đại từ nghi vấn
-  class IntrproWord < PronounWord
-  end
-
-  class ProNa2 < IntrproWord
-    def noun_prefix?
-      false
-    end
+  def noun_prefix?
+    false
   end
 end
--- old/mtlv2/engine/mt_node/verbal.cr
+++ old/mtlv2/engine/mt_node/verbal.cr
@@ -1,288 +1,69 @@
-require "./nominal"
-require "./adverbial"
+def self.from_key(key : String)
+  attr = self.can_chain?(key) ? Chain : None
+  return attr if key.size == 1
 
-module MT::MTL
-  @[Flags]
-  enum VerbAttr
-    # shared
-    DirCompl # Direction complement
-    ResCompl # Result complement
-    PotCompl # Potential complement
-    QtiCompl # Quantity complement
-
-    # HasPzai
-    HasUzhe
-    # HasUde3
-    # HasBu4
-    HasUle
-
-    Basic
-    Chain # serial-verb
-    Req2O # need 2 objects
-
-    # Auxil
-    Intrv    # intransitive
-    Polysemy # both intra + trans
-    Vobjt    # verb + object pair
-
-    def needs_obj?
-      value & (Intrv | Vobjt) == 0
-    end
-
-    def not_needs_obj?
-      value & (Intrv | Polysemy | Vobjt) != 0
-    end
-
-    def self.from(tag : String, key : String) : self
-      case tag[1]?
-      when nil then from_key(key)
-      when 'o' then Vobjt
-      when 'i' then Intrv
-      when 'j' then Polysemy
-      when '2' then Req2O
-      else          None
-      end
-    end
-
-    def self.from_key(key : String)
-      attr = self.can_chain?(key) ? Chain : None
-      return attr if key.size == 1
-
-      case key[-1]?
-      when '了' then attr | HasUle
-        # when '在' then attr |= HasPzai
-        # when '得' then attr |= HasUde3
-      when '着' then attr | HasUzhe | Chain
-      when '下', '上', '出', '进', '回',
-           '过', '起', '来', '去'
-        attr | DirCompl
-      when '好', '完', '错', '晚', '坏',
-           '饱', '清', '到', '走', '会',
-           '懂', '见', '掉'
-        attr | ResCompl
-      else
-        attr
-      end
-    end
-
-    def self.can_chain?(key : String)
-      {'来', '去', '到', '出'}.includes?(key[0])
-    end
-  end
-
-  module Verbal
-    getter attr = VerbAttr::None
-    forward_missing_to @attr
-
-    def self.from_term(term : V2Term, pos = 0)
-      tag = term.tags[pos]? || "v"
-      case tag[1]?
-      when 'p' then VcmpWord.new(term, pos)
-      when 'f' then VdirWord.new(term, pos)
-      when 'm' then VmodWord.new(term, pos)
-      when 'x' then VauxWord.new(term, pos)
-      when 'j' then VmixWord.new(term, pos)
-      when '!' then unique_verb(term, pos)
-      else          VerbWord.new(term, pos)
-      end
-    end
-
-    def self.unique_verb(term : V2Term, pos = 0)
-      case term.key[-1]?
-      when '会' then VmodHui.new(term, pos)
-      when '能' then VmodNeng.new(term, pos)
-      when '想' then VmodXiang.new(term, pos)
-      when '是' then VShiWord.new(term, pos)
-      when '有' then VYouWord.new(term, pos)
-      else          VerbWord.new(term, pos)
-      end
-    end
-  end
-
-  # 动词 - verb - động từ
-  class VerbWord < BaseWord
-    include Verbal
-
-    def initialize(term : V2Term, pos : Int32 = 0)
-      super(term, pos)
-      tag = term.tags[pos]? || "v"
-      @attr = VerbAttr.from(tag, term.key)
-    end
-  end
-
-  class VmixWord < VerbWord
-    @trans_val : String
-
-    def initialize(term : V2Term, pos : Int32 = 0)
-      super(term, pos)
-      @trans_val = term.vals[pos &+ 1]? || @val
-    end
-
-    def as_trans!
-      @val = @trans_val
-      self
-    end
-  end
-
-  class VdirWord < VerbWord
-    # MAP_VAL = {
-    #   "上"  => "lên",
-    #   "下"  => "xuống",
-    #   "进"  => "vào",
-    #   "出"  => "ra",
-    #   "过"  => "qua",
-    #   "去"  => "đi",
-    #   "回"  => "trở về",
-    #   "起"  => "lên",
-    #   "来"  => "tới",
-    #   "上来" => "đi đến",
-    #   "上去" => "đi lên",
-    #   "下来" => "lại",
-    #   "下去" => "xuống",
-    #   "进来" => "tiến đến",
-    #   "进去" => "đi vào",
-    #   "出来" => "đi ra",
-    #   "出去" => "ra ngoài",
-    #   "过来" => "qua tới",
-    #   "过去" => "qua",
-    #   "回来" => "trở về",
-    #   "回去" => "trở lại",
-    #   "起来" => "lên",
-    # }
-
-    include Verbal
-    @dir_val : String
-
-    def initialize(term : V2Term, pos : Int32 = 0)
-      super(term, pos)
-      @attr |= VerbAttr::Chain if VerbAttr.can_chain?(term.key)
-
-      @dir_val = term.vals[pos &+ 1]? || @val
-    end
-
-    def as_dir!
-      @val = @dir_val
-    end
-  end
-
-  class VcmpWord < VerbWord
-    # COMPARE = {
-    #   "如"  => "tựa",
-    #   "像"  => "giống",
-    #   "仿佛" => "giống",
-    #   "宛若" => "giống",
-    #   "好像" => "thật giống",
-    # }
-
-    @cmp_val : String
-
-    def initialize(term : V2Term, pos : Int32 = 0)
-      super(term, pos)
-      @cmp_val = term.vals[pos &+ 1]? || @val
-    end
-
-    def as_cmp!
-      @val = @cmp_val
-    end
-  end
-
-  class VauxWord < VerbWord
-    # 形式动词 - pro-verb - động từ hình thái
-
-    # HELPING = {
-    #   "爱"   => "yêu",
-    #   "喜欢"  => "thích",
-    #   "避免"  => "tránh",
-    #   "加以"  => "tiến hành",
-    #   "进行"  => "tiến hành",
-    #   "予以"  => "ban cho",
-    #   "着手"  => "lấy tay",
-    #   "舍得"  => "nỡ bỏ",
-    #   "忍不住" => "không nhịn được",
-    # }
-
-    @aux_val : String
-
-    def initialize(term : V2Term, pos : Int32 = 0)
-      super(term, pos)
-      @aux_val = term.vals[pos &+ 1]? || @val
-    end
-
-    def as_aux!
-      @val = @aux_val
-    end
-  end
-
-  class VmodWord < VerbWord
-    include Verbal
-
-    def initialize(term : V2Term, pos : Int32 = 0)
-      super(term, pos)
-    end
-  end
-
-  class VmodHui < VmodWord
-  end
-
-  class VmodNeng < VmodWord
-    # động từ `năng`
-  end
-
-  class VmodXiang < VmodWord
-    # động từ `tưởng`
-  end
-
-  #################
-
-  class VShiWord < BaseWord
-    include Verbal
-  end
-
-  class VYouWord < BaseWord
-    include Verbal
-  end
-
-  ####
-
-  class VerbPair < PairNode
-    include Verbal
-  end
-
-  class VerbForm
-    include SeriNode
-    include Verbal
-
-    getter verb : Verbal
-    getter advb : MtNode? = nil   # advebial form
-    getter prep : PrepForm? = nil # prepos phrase
-
-    def initialize(@verb : Verbal, @attr = verb.attr)
-      verb.bequest!(self)
-    end
-
-    def add_advb(@advb : MtNode) : Nil
-      self.set_prev(advb.prev?)
-      advb.set_prev(nil)
-    end
-
-    def add_prep(@prep : MtNode) : Nil
-      self.set_prev(prep.prev?)
-      prep.set_prev(nil)
-    end
-
-    def each
-      if (prep = @prep) && !prep.postpos?
-        yield prep
-        prep = nil
-      end
-
-      if advb = @advb
-        # TODO: check if advb should place after
-        yield advb
-      end
-
-      yield verb
-      yield prep if prep
-    end
+  case key[-1]?
+  when '了' then attr | HasUle
+    # when '在' then attr |= HasPzai
+    # when '得' then attr |= HasUde3
+  when '着' then attr | HasUzhe | Chain
+  when '下', '上', '出', '进', '回',
+       '过', '起', '来', '去'
+    attr | DirCompl
+  when '好', '完', '错', '晚', '坏',
+       '饱', '清', '到', '走', '会',
+       '懂', '见', '掉'
+    attr | ResCompl
+  else
+    attr
   end
 end
+
+def self.vlinking?(key : String)
+  {'来', '去', '到', '出'}.includes?(key[0])
+end
+
+DIRECTION = {
+  "上"  => "lên",
+  "下"  => "xuống",
+  "进"  => "vào",
+  "出"  => "ra",
+  "过"  => "qua",
+  "去"  => "đi",
+  "回"  => "trở về",
+  "起"  => "lên",
+  "来"  => "tới",
+  "上来" => "đi đến",
+  "上去" => "đi lên",
+  "下来" => "lại",
+  "下去" => "xuống",
+  "进来" => "tiến đến",
+  "进去" => "đi vào",
+  "出来" => "đi ra",
+  "出去" => "ra ngoài",
+  "过来" => "qua tới",
+  "过去" => "qua",
+  "回来" => "trở về",
+  "回去" => "trở lại",
+  "起来" => "lên",
+}
+
+COMPARE = {
+  "如"  => "tựa",
+  "像"  => "giống",
+  "仿佛" => "giống",
+  "宛若" => "giống",
+  "好像" => "thật giống",
+}
+
+AUXILS = {
+  "爱"   => "yêu",
+  "喜欢"  => "thích",
+  "避免"  => "tránh",
+  "加以"  => "tiến hành",
+  "进行"  => "tiến hành",
+  "予以"  => "ban cho",
+  "着手"  => "lấy tay",
+  "舍得"  => "nỡ bỏ",
+  "忍不住" => "không nhịn được",
+}
