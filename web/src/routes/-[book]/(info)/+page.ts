867fd4483e360fd2efab240d63b586a94d71c083
Nipinium <nipinium@gmail.com>
2022-11-05 10:56:53 +0700

    add missing right nav

--- web/src/routes/-[book]/(info)/+page.ts
+++ web/src/routes/-[book]/(info)/+page.ts
@@ -1,4 +1,5 @@
 import { get_crits } from '$lib/ys_api'
+import { suggest_read } from '$utils/ubmemo_utils'
 
 async function load_crits(book_id: number, fetch = globalThis.fetch) {
   const opts = { book: book_id, take: 3, sort: 'score' }
@@ -7,7 +8,7 @@ async function load_crits(book_id: number, fetch = globalThis.fetch) {
 }
 
 export async function load({ fetch, parent }) {
-  const { nvinfo } = await parent()
+  const { nvinfo, ubmemo } = await parent()
 
   const bhash = nvinfo.bslug.substring(0, 8)
   const api_url = `/api/books/${bhash}/front`
@@ -23,6 +24,7 @@ export async function load({ fetch, parent }) {
       // prettier-ignore
       { text: nvinfo.btitle_vi, icon: 'book', href: `/-${nvinfo.bslug}`, "data-show": 'tm', "data-kind": 'title' },
     ],
+    right_nav: [suggest_read(nvinfo, ubmemo)],
   }
 
   return { ...payload.props, crits, _meta }
