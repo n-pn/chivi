d6c952024572f2f1b3987383bb70915df46e6cfa
Nipinium <nipinium@gmail.com>
2023-01-12 18:38:25 +0700

    tweak nodemon config

--- priv/invoke-dev.sh
+++ priv/invoke-dev.sh
@@ -4,15 +4,20 @@ set -euo pipefail
 CV_ENV=development
 AMBER_ENV=development
 
-sudo service $1-srv stop
-echo killed service $1-srv to start $1-srv in dev mode!
+TARGET=${1:-"cvapp"}
+SOURCE=$TARGET
+WATCH="--watch ./src/_util --watch ./src/$SOURCE"
 
 if [[ $1 == "cvapp" ]]
 then
-nodemon -L --watch ./src/webcv --exec "crystal run" src/webcv/webcv_srv.cr --error-trace
-else
-nodemon -L --watch ./src/$1 --exec "crystal run" src/$1/$1_srv.cr --error-trace
+  SOURCE="webcv"
+  WATCH="--watch ./src/_util --watch ./src/webcv --watch ./src/appcv"
 fi
 
-echo turn back to $1-srv service!
-sudo service $1-srv restart
+sudo service $TARGET-srv stop
+echo killed service $TARGET-srv to start $TARGET-srv in dev mode!
+
+nodemon $WATCH --exec "crystal run --error-trace" src/$SOURCE/${SOURCE}_srv.cr
+
+# echo turn back to $TARGET-srv service!
+# sudo service $TARGET-srv start
