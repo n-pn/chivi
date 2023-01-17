1500ce8fbdebab3ff4bd7aad9e74fd81f76e3ce2
Nipinium <nipinium@gmail.com>
2023-01-17 14:08:10 +0700

    map v1 data to sqlite3

---
+++ src/mt_v1/data/gen/gen_v1_dicts.cr
@@ -0,0 +1,63 @@
+# Dir.mkdir_p("var/dicts/v1raw")
+
+require "../v1_dict"
+
+basics = {
+  {1, "regular", "Thông dụng", "Từ điển chung cho tất cả các bộ truyện", 2},
+  {2, "essense", "Nền tảng", "Từ điển cơ sở chung cho các chế độ dịch", 3},
+  {3, "fixture", "Khoá cứng", "Các từ nghĩa cố định dùng trong máy dịch", 3},
+  {4, "combine", "Tổng hợp", "Từ điển tổng hợp dùng cho chế độ dịch nhanh", 1},
+}
+
+others = {
+  {10, "hanviet", "Hán việt", "Từ điển phiên âm Hán Việt", 3},
+  {11, "pin_yin", "Bính âm", "Từ điển bính âm (pinyin)", 3},
+  {12, "surname", "Họ tiếng Trung", "Từ điển họ phổ biến tiếng Trung", 3},
+}
+
+cvmtls = {
+  {20, "fix_nouns", "Sửa danh từ", "Nghĩa của từ khi nó là danh từ", 3},
+  {21, "fix_verbs", "Sửa động từ", "Nghĩa của từ khi nó là động từ", 3},
+  {22, "fix_adjts", "Sửa tính từ", "Nghĩa của từ khi nó là tính từ", 3},
+  {23, "fix_advbs", "Sửa phó từ", "Nghĩa của từ khi nó là phó từ", 3},
+  {24, "fix_u_zhi", "Sửa sau 之", "Đổi nghĩa của vế phải cụm từ kết hợp bởi 之", 3},
+  {25, "qt_nouns", "Danh lượng từ", "Lượng từ dành riêng cho danh từ", 3},
+  {26, "qt_verbs", "Động lượng từ", "Lượng từ dành riêng cho động từ", 3},
+  {27, "qt_times", "Thời lượng từ", "Lượng từ chỉ thời gian", 3},
+  {28, "v_dircom", "Bổ ngữ xu hướng", "Nghĩa của từ khi làm bổ ngữ xu hướng", 3},
+  {29, "v_rescom", "Bổ ngữ kết quả", "Các loại bổ ngữ đứng sau động từ khác", 3},
+  {30, "v_ditran", "Động từ 2 tân", "Động từ yêu cầu 2 tân ngữ, tân ngữ trước thường chỉ người", 3},
+  {31, "verb_obj", "Động từ ly hợp", "Cụm động tân có thể tạo thành cấu trúc ly hợp, biểu hiện kỹ năng", 3},
+}
+
+basics.each { |x| M1::DbDict.new(*x, dtype: 0).save! }
+others.each { |x| M1::DbDict.new(*x, dtype: 1).save! }
+cvmtls.each { |x| M1::DbDict.new(*x, dtype: 2).save! }
+
+require "../../../config"
+require "pg"
+
+dicts = [] of M1::DbDict
+
+DB.open(CV::Config.database_url) do |db|
+  query = <<-SQL
+    select id, bhash, vname from nvinfos where id > 0 order by id asc
+  SQL
+
+  db.query_each(query) do |rs|
+    id, bhash, bname = rs.read(Int64, String, String)
+
+    dicts << M1::DbDict.new(
+      id: -id.to_i,
+      dname: "-" + bhash,
+      label: bname,
+      brief: "Từ điển riêng cho bộ truyện [#{bname}]",
+      privi: 1,
+      dtype: 3
+    )
+  end
+end
+
+M1::DbDict.repo.open_tx do |db|
+  dicts.each(&.save!(db))
+end
---
+++ src/mt_v1/data/v1_dict.cr
@@ -0,0 +1,132 @@
+require "crorm"
+require "crorm/sqlite3"
+require "sqlite3"
+
+class M1::DbDict
+  include Crorm::Model
+  @@table = "dicts"
+
+  field id : Int32, primary: true
+
+  field dname : String
+  field label : String = ""
+  field brief : String = ""
+
+  field privi : Int32 = 0
+  field dtype : Int32 = 0
+
+  field term_total : Int32 = 0
+  field term_avail : Int32 = 0
+
+  field base_terms : Int32 = 0
+  field temp_terms : Int32 = 0
+  field user_terms : Int32 = 0
+
+  field users : String = ""
+  field mtime : Int64 = 0
+
+  def initialize(@id, @dname, @label = "", @brief = "", @privi = 1, @dtype = 0)
+  end
+
+  def save!
+    fields, values = get_changes
+
+    query = Crorm::Sqlite3::SQL.upsert_sql(@@table, fields) do |sql|
+      fields = {"dname", "label", "brief", "privi", "dtype"}
+      Crorm::Sqlite3::SQL.build_upsert_sql(sql, fields)
+      sql << " where dname == excluded.dname"
+    end
+
+    self.class.repo.open_tx(&.exec query, args: values)
+  end
+
+  def update!(changes : Hash(String, DB::Any))
+    update!(changes.keys, changes.value)
+  end
+
+  def update!(fields : Enumerable(String), values : Enumerable(DB::Any))
+    values << self.id!
+    self.class.repo.update(@table, fields, values) { |sql| sql << " where id = ?" }
+  end
+
+  def update_after_term_added!(@mtime, @term_total)
+    repo = self.class.repo
+    repo.db.exec "update dicts set mtime = ?, term_total = ?  where id = ?", mtime, term_total, self.id!
+  end
+
+  def update_after_term_added!(@mtime)
+    repo = self.class.repo
+    repo.db.exec "update dicts set  mtime = ?, term_total = term_total + 1 where id = ?", mtime, self.id!
+    @term_total = repo.db.query_one("select term_total from dicts where id = ?", self.id!, as: Int32)
+  end
+
+  def tuple
+    {@dname, @label, @term_total, @brief}
+  end
+
+  ######
+
+  class_getter repo : Crorm::Sqlite3::Repo { Crorm::Sqlite3::Repo.new(self.db_path, self.init_sql) }
+
+  @[AlwaysInline]
+  def self.db_path
+    "var/dicts/v1raw/dicts_v1.db"
+  end
+
+  def self.init_sql
+    {{ read_file("#{__DIR__}/sql/dicts_v1.sql") }}
+  end
+
+  ####
+
+  DICT_IDS = {} of String => Int32
+
+  def self.get_id(dname : String) : Int32
+    DICT_IDS[dname] ||= begin
+      query = "select id from dicts where dname = ?"
+      self.repo.open_db(&.query_one?(query, dname, as: Int32)) || 0
+    end
+  end
+
+  def self.get!(id : Int32) : self
+    query = "select * from dicts where id = ?"
+    self.repo.open_db(&.query_one(query, id, as: self))
+  end
+
+  def self.get!(dname : String) : self
+    query = "select * from #{@@table} where dname = ?"
+    self.repo.open_db(&.query_one(query, dname, as: self))
+  end
+
+  def self.get(dname : String) : self | Nil
+    find!(dname) rescue nil
+  end
+
+  def self.count : Int32
+    query = "select count(*) from #{@@table} where term_total > 0"
+    @@repo.open_db(&.query_one(query, as: Int32))
+  end
+
+  def self.bdicts_count : Int32
+    query = "select count(*) from #{@@table} where term_total > 0 and id < 0"
+    @@repo.open_db(&.query_one(query, as: Int32))
+  end
+
+  def self.all(limit : Int32, offset = 0) : Array(self)
+    query = <<-SQL
+      select * from #{@@table} where term_total > 0
+      order by mtime desc limit ? offset ?
+    SQL
+
+    @@repo.open_db(&.query_all(query, limit, offset, as: self))
+  end
+
+  def self.bdicts_all(limit : Int32, offset = 0)
+    query = <<-SQL
+      select * from #{@@table} where id < 0 and term_total > 0
+      order by mtime desc limit ? offset ?
+    SQL
+
+    @@repo.open_db(&.query_all(query, limit, offset, as: self))
+  end
+end
