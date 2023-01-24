3e95e7ea88069d7c7b766db794956d34a4f45d5c
Nipinium <nipinium@gmail.com>
2023-01-25 02:06:26 +0700

    wip: import rules

--- src/mt_v2/data/sql/rules_v2.sql
+++ src/mt_v2/data/sql/rules_v2.sql
@@ -1,7 +1,6 @@
-pragma journal_mode = WAL;
 
 CREATE TABLE IF NOT EXISTS rules (
-  id integer PRIMARY KEY,
+  id integer PRIMARY KEY AUTOINCREMENT,
   -- input
   rule varchar NOT NULL DEFAULT '', -- array of postag id seperated by ' + '
   size integer NOT NULL DEFAULT 0, -- number of items in rule input
@@ -10,20 +9,22 @@ CREATE TABLE IF NOT EXISTS rules (
   desc text NOT NULL DEFAULT '',
   note text NOT NULL DEFAULT '',
   -- output
-  kind integer NOT NULL DEFAULT 0, -- output kind
   ptag integer NOT NULL DEFAULT 0, -- output ptag
-  combine varchar NOT NULL DEFAULT '', -- output type is combine nodes
-  replace varchar NOT NULL DEFAULT '', -- output type is replace nodes
+  kind integer NOT NULL DEFAULT 0, -- output kind
+  _combine varchar NOT NULL DEFAULT '', -- output type is combine nodes
+  _replace varchar NOT NULL DEFAULT '', -- output type is replace nodes
   -- extras
   --
   uname varchar NOT NULL DEFAULT '', -- created by
   mtime bigint NOT NULL DEFAULT 0, -- created/updated at
   --
-  _flag integer NOT NULL DEFAULT 0, -- mark active/inactive
+  _flag integer NOT NULL DEFAULT 0 -- mark active/inactive
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS rules_rule_idx ON rules (rule);
+CREATE UNIQUE INDEX IF NOT EXISTS rules_rule_idx ON rules (rule, ptag);
 
 CREATE INDEX IF NOT EXISTS rules_ptag_idx ON rules (ptag);
 
 CREATE INDEX IF NOT EXISTS rules_user_idx ON rules (uname);
+
+pragma journal_mode = WAL;
