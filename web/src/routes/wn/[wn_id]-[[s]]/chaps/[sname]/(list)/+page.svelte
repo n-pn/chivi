9eb87bf394d8cab3dfd9cf4f8596161cae57c153
Nipinium <nipinium@gmail.com>
2023-01-29 15:46:59 +0700

    hide reload button if no remote seeds

--- web/src/routes/wn/[wn_id]-[[s]]/chaps/[sname]/(list)/+page.svelte
+++ web/src/routes/wn/[wn_id]-[[s]]/chaps/[sname]/(list)/+page.svelte
@@ -90,16 +90,18 @@
         <span class="-hide">Thêm text</span>
       </a>
 
-      <button
-        class="m-btn _primary"
-        disabled={!can_reload}
-        on:click={reload_seed}
-        data-tip="Cập nhật danh sách chương tiết từ nguồn ngoài"
-        data-tip-loc="bottom"
-        data-tip-pos="right">
-        <SIcon name={_onload ? 'loader' : 'refresh'} spin={_onload} />
-        <span class="-hide">Đổi mới</span>
-      </button>
+      {#if seed_data.links.length > 0}
+        <button
+          class="m-btn _primary"
+          disabled={!can_reload}
+          on:click={reload_seed}
+          data-tip="Cập nhật danh sách chương tiết từ nguồn ngoài"
+          data-tip-loc="bottom"
+          data-tip-pos="right">
+          <SIcon name={_onload ? 'loader' : 'refresh'} spin={_onload} />
+          <span class="-hide">Đổi mới</span>
+        </button>
+      {/if}
 
       <Gmenu dir="right">
         <button class="m-btn" slot="trigger">
