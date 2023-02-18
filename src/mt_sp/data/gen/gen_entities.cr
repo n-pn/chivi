b869ca81b59e5db43e7af51b612b5bd79d9a0af7
Nipinium <nipinium@gmail.com>
2023-02-19 02:52:32 +0700

    cleanup entity file

--- src/mt_sp/data/gen/gen_entities.cr
+++ src/mt_sp/data/gen/gen_entities.cr
@@ -2,49 +2,104 @@ require "json"
 require "colorize"
 require "../sp_ents"
 
-DIR = "/mnt/devel/Chivi/results/entities"
-files = Dir.glob("#{DIR}/*/*.tsv")
+DIR = "var/texts/anlzs/tmp"
 
-SP::EntRelate.init_db(reset: true)
-SP::EntValue.init_db(reset: true)
-SP::EntFreq.init_db(reset: true)
+# SP::EntRelate.init_db(reset: false)
+# SP::EntValue.init_db(reset: false)
+# SP::EntFreq.init_db(reset: false)
 
-SP::EntRelate.db.exec "begin"
-SP::EntValue.db.exec "begin"
-SP::EntFreq.db.exec "begin"
+relates_sql = "select form, etag from ent_relates"
+values_sql = "select form, etag from ent_values"
+
+known_relates = SP::EntRelate.db.query_all(relates_sql, as: {String, String}).to_set
+known_values = SP::EntValue.db.query_all(values_sql, as: {String, String}).to_set
+
+books = Dir.children(DIR).sort_by! do |book|
+  rows = book.split('-')
+  book.starts_with?('!') ? rows.last.to_i - 100000 : rows.first.to_i
+end
+
+PRUNE = ARGV.includes?("--prune")
+
+books.each do |book|
+  SP::EntRelate.db.exec "begin"
+  SP::EntValue.db.exec "begin"
+
+  inp_dir = File.join(DIR, book)
+  next unless File.directory?(inp_dir)
+
+  files = Dir.children(inp_dir).select!(&.ends_with?("fine[M].tsv"))
+  puts "#{inp_dir}: #{files.size}"
+
+  files.each do |file|
+    inp_file = File.join(inp_dir, file)
+    strio = String::Builder.new
+
+    File.each_line(inp_file) do |line|
+      rows = line.split('\t')
+
+      if rows.size < 3
+        strio << '\n' if line.empty?
+        next
+      end
+
+      form, idx, etag = rows
+      strio << form << '\t' << idx << '\t' << etag << '\n'
+
+      next unless (meaning = rows[4]?) && !meaning.empty?
+
+      add_relates(form, etag, meaning, known_relates)
+      add_values(form, etag, meaning, known_values)
+    rescue
+      puts [inp_file, line].colorize.red
+      exit 1
+    end
+
+    File.write(inp_file.sub(".tsv", ".ner"), strio.to_s)
+  end
+
+  SP::EntRelate.db.exec "commit"
+  SP::EntValue.db.exec "commit"
+
+  next unless PRUNE
+
+  files.each do |file|
+    inp_file = File.join(inp_dir, file)
+    File.delete?(inp_file)
+
+    puts "#{inp_file} deleted".colorize.yellow
+  end
+end
 
 struct Related
   include JSON::Serializable
   getter related : Array(String)
 end
 
-files.each do |file|
-  puts file
+def add_relates(form, etag, meaning, known)
+  return if known.includes?({form, etag}) || !meaning.includes?(%q|"related":|)
 
-  etag = File.basename(file, ".tsv")
+  puts "new related data added".colorize.blue
 
-  File.each_line(file) do |line|
-    form, meaning = line.split('\t')
+  related = Related.from_json(meaning).related
 
-    SP::EntFreq.upsert(form, etag, 1)
-
-    next if meaning.empty?
-
-    if meaning.includes?(%q|"related":|)
-      related = Related.from_json(meaning).related.join('\t')
-      SP::EntRelate.upsert(form, etag, related)
-    end
-
-    if meaning.includes?(%q|"value":|)
-      SP::EntValue.upsert(form, etag, meaning)
-    end
-  rescue
-    puts file.colorize.red
-    puts line.colorize.red
-    exit 1
+  File.open("tmp/ent_relates.tsv", "a") do |file|
+    file << form << '\t' << etag << '\t' << related.to_json
   end
+
+  SP::EntRelate.upsert(form, etag, related.join('\t'))
+  known << {form, etag}
 end
 
-SP::EntRelate.db.exec "commit"
-SP::EntValue.db.exec "commit"
-SP::EntFreq.db.exec "commit"
+def add_values(form, etag, meaning, known)
+  return if known.includes?({form, etag}) || !meaning.includes?(%q|"value":|)
+  puts "new value data added".colorize.red
+
+  File.open("tmp/ent_values.tsv", "a") do |file|
+    file << form << '\t' << etag << '\t' << meaning
+  end
+
+  SP::EntValue.upsert(form, etag, meaning)
+
+  known << {form, etag}
+end
