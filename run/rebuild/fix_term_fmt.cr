918138662afb37e3ca2d523befbdd80922b39875
NPN <me@npn.am>
2023-04-27 06:31:19 +0700

    fix fmt

--- run/rebuild/fix_term_fmt.cr
+++ run/rebuild/fix_term_fmt.cr
@@ -4,13 +4,15 @@ DIC = DB.open("sqlite3:#{MT::MtDefn.db_path("common-main")}")
 at_exit { DIC.close }
 
 input = DIC.query_all <<-SQL, as: String
-  select zstr from defns where xpos = 'PU'
+  select zstr from defns where xpos = 'PU' or upos = 'w'
   SQL
 
 puts input
 
 DIC.exec "begin"
 input.each do |zstr|
+  zstr = zstr.chars.map { |char| CharUtil.normalize(char) }.join
+
   fmt = MT::FmtFlag.auto_detect(zstr)
   DIC.exec "update defns set _fmt = $1 where zstr = $2", fmt.to_i, zstr
 end
