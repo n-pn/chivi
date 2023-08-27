928c4f2c3034a5962420f0d9d95c04ae63a068f3
NPN <me@npn.am>
2023-08-28 01:15:24 +0700

    fix preload html script

--- run/wnstem/check_latest_bid.cr
+++
@@ -1,7 +0,0 @@
-require "../../src/wnapp/util/dl_host"
-
-hostname = ARGV[0]? || "xbiquge.bz"
-dlhost = DlHost.load_by_name(hostname) { exit 1 }
-
-last_bid = dlhost.get_last_bid.try(&.to_i) || raise "can't find latest book id"
-puts "Latest book id for #{hostname.colorize.yellow}: #{last_bid.colorize.yellow}"
--- run/wnstem/preload_hetushu_chaps.cr
+++ run/wnstem/preload_hetushu_chaps.cr
@@ -1,29 +1,12 @@
 require "sqlite3"
-require "../../src/tasks/remote/shared/rmseed"
+require "colorize"
+require "../../src/zroot/html_parser/rmconf"
 
-DB3_DIR = "/2tb/var.chivi/zchap/globs/!hetushu"
-HTM_DIR = "/2tb/var.chivi/.keep/wnchap/www.hetushu.com"
-TXT_DIR = "var/texts/rgbks/!hetushu.com"
+DB3_DIR = "var/zroot/wnchap/!hetushu"
+HTM_DIR = "var/.keep/rmchap/!hetushu"
 
-def preload_book(seed : Rmseed, bid : Int32)
-  db_path = "#{DB3_DIR}/#{bid}.db3"
-  return unless File.file?(db_path)
-
-  all_ids = DB.open("sqlite3:#{db_path}", &.query_all("select s_cid from chaps", as: String))
-
-  total = all_ids.size
-
-  txt_dir = "#{TXT_DIR}/#{bid}"
-  all_ids -= Dir.children(txt_dir).map!(&.sub(".gbk", "")) if File.directory?(txt_dir)
-
-  htm_dir = "#{HTM_DIR}/#{bid}"
-  Dir.mkdir_p(htm_dir)
-
-  all_ids -= Dir.children(htm_dir).map!(&.sub(".htm", ""))
-
-  puts "- total: #{total}, missing: #{all_ids.size}"
-
-  q_size = all_ids.size
+def fetch_html(conf : Rmconf, queue : Array({String, String, Int32}))
+  q_size = queue.size
   w_size = q_size
   w_size = 6 if w_size > 6
 
@@ -34,9 +17,8 @@ def preload_book(seed : Rmseed, bid : Int32)
     spawn do
       loop do
         save_path, chap_link, idx = workers.receive
-
-        seed.save_page(chap_link, save_path)
-        puts "- #{idx}/#{q_size}  #{chap_link.colorize.blue} => [#{save_path}]"
+        conf.save_page(chap_link, save_path)
+        puts "- #{idx}/#{q_size}  #{chap_link.colorize.blue} saved!"
       rescue err
         Log.error(exception: err) { err.message.colorize.red }
       ensure
@@ -45,17 +27,86 @@ def preload_book(seed : Rmseed, bid : Int32)
     end
   end
 
-  all_ids.each_with_index(1) do |cid, idx|
-    save_path = "#{htm_dir}/#{cid}.htm"
-    chap_path = seed.conf.make_chap_path(bid, cid)
-    workers.send({save_path, chap_path, idx})
-  end
-
+  queue.each { |entry| workers.send(entry) }
   q_size.times { results.receive }
 end
 
-seed = Rmseed.from("www.hetushu.com")
+def fetch_json(conf : Rmconf, queue : Array({String, String, Int32}))
+  q_size = queue.size
+  w_size = q_size
+  w_size = 6 if w_size > 6
 
-seed.get_mbid.to_i.downto(1) do |bid|
+  workers = Channel({String, String, Int32}).new(q_size)
+  results = Channel(Nil).new(w_size)
+
+  w_size.times do
+    spawn do
+      loop do
+        chap_path, chap_link, idx = workers.receive
+
+        json_path = chap_path.sub(".htm", ".tok")
+        json_link = chap_link.sub(/(\d+).html$/) { "r#{$1}.json" }
+
+        conf.http_client.get(json_link, headers: conf.xhr_headers(chap_link)) do |res|
+          raise "Can't download decode string" unless res.success?
+          res.headers["token"].tap { |x| File.write(json_path, x) }
+        end
+
+        puts "- #{idx}/#{q_size} #{json_link.colorize.blue} saved!"
+      rescue err
+        Log.error(exception: err) { err.message.colorize.red }
+      ensure
+        results.send(nil)
+      end
+    end
+  end
+
+  queue.each { |entry| workers.send(entry) }
+  q_size.times { results.receive }
+end
+
+def preload_book(conf : Rmconf, bid : Int32)
+  db_path = "#{DB3_DIR}/#{bid}.db3"
+  return unless File.file?(db_path)
+
+  query = "select spath from chinfos where cksum = ''"
+  spaths = DB.open("sqlite3:#{db_path}", &.query_all(query, as: String))
+
+  if spaths.empty?
+    puts "- #{bid} all chap saved, skipping"
+    return
+  end
+
+  sc_ids = spaths.map { |x| File.basename(x) }
+  to_save = sc_ids.size
+
+  htm_dir = "#{HTM_DIR}/#{bid}"
+
+  if File.directory?(htm_dir)
+    sc_ids -= Dir.children(htm_dir).map!(&.sub(".tok", ""))
+  else
+    Dir.mkdir_p(htm_dir)
+  end
+
+  if spaths.empty?
+    puts "- #{bid} all chap cached, skipping"
+    return
+  end
+
+  puts "- total: #{to_save}, missing: #{sc_ids.size}"
+
+  queue = sc_ids.map_with_index(1) do |cid, idx|
+    save_path = "#{htm_dir}/#{cid}.htm"
+    chap_path = conf.make_chap_path(bid, cid)
+    {save_path, chap_path, idx}
+  end
+
+  fetch_html(conf, queue)
+  fetch_json(conf, queue)
+end
+
+seed = Rmconf.from_host!("www.hetushu.com")
+
+seed.get_max_bid.to_i.downto(1) do |bid|
   preload_book(seed, bid)
 end
