a99a5ed7bbdf792c4f17f5015221a5942efe880c
NPN <me@npn.am>
2023-12-02 10:39:46 +0700

    delete dead routes

--- web/src/routes/rm/[sn]:[id]/(info)/+layout.svelte
+++ web/src/routes/rm/[sn]:[id]/(info)/+layout.svelte
@@ -25,25 +25,10 @@
       { type: 'gd', href: `${sroot}/gd`, icon: 'message', text: 'Thảo luận' },
       // { type: 'st', href: `${sroot}/st`, icon: 'activity', text: 'Thay đổi' },
     ],
-    ul: [
-      { type: 'zh', href: `${sroot}/ul`, icon: 'upload', text: 'Text gốc' },
-      { type: 'tl', href: `${sroot}/ul/tran`, icon: 'language', text: 'Bản dịch' },
-      { type: 'md', href: `${sroot}/ul/data`, icon: 'tree', text: 'Ngữ pháp' },
-      // { type: 'vd', href: `${sroot}/ul/term`, icon: 'package', text: 'Từ điển' },
-    ],
-    su: [
-      { type: 'cf', href: `${sroot}/su`, icon: 'settings', text: 'Thiết đặt' },
-      { type: 'rm', href: `${sroot}/su/redo`, icon: 'eraser', text: 'Tẩy xóa' },
-      { type: 'lg', href: `${sroot}/su/xlog`, icon: 'lock-open', text: 'Mở khóa' },
-    ],
+
   }
 
   $: ({ intab = 'rd', ontab = 'ch' } = $page.data)
-
-  const trigger_liked = async () => {
-    $rmemo.rd_track = $rmemo.rd_track < 1 ? data._user.privi + 1 : 0
-    $rmemo = await upsert_memo($rmemo, 'rd_track')
-  }
 </script>
 
 <RmstemFull {rstem} binfo={data.binfo || null} />
